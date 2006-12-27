462974c31664c37d4e398c77ac203e051429ea4a
Vlad Dumitrescu <vladdu55@gmail.com>
2006-12-27 20:33:46 +0000

    spelling

--- org.erlide.ui/src/org/erlide/ui/editors/erl/EditorConfiguration.java
+++ org.erlide.ui/src/org/erlide/ui/editors/erl/EditorConfiguration.java
@@ -2,7 +2,8 @@
  * Copyright (c) 2004 Eric Merritt and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *     Eric Merritt
