026a3c823c61dd6a9bbea7d25b22725404953469
Vlad Dumitrescu <vladdu55@gmail.com>
2007-03-16 15:03:08 +0000

    fixed

--- org.erlide.build/build.bat
+++ org.erlide.build/build.bat
@@ -2,6 +2,6 @@ set ECLIPSE_HOME=%1
 set ERLANG_HOME=%2
 set WORKSPACE=%3
 
-java -jar %ECLIPSE_HOME%/startup.jar -application org.eclipse.ant.core.antRunner -Dbuilder=%WORKSPACE%/erlide-build -DlocalWorkspace=%WORKSPACE% -DerlRoot=%ERLANG_HOME% %4 %5 %6 %7 %8 %9
+java -jar %ECLIPSE_HOME%/startup.jar -application org.eclipse.ant.core.antRunner -Dbuilder=%WORKSPACE%/org.erlide.build -DlocalWorkspace=%WORKSPACE% -DerlRoot=%ERLANG_HOME% %4 %5 %6 %7 %8 %9
 
 
