32d51d7ae44196d59689f3929ea704803cb8b017
Jakob Cederlund <jakobce@gmail.com>
2007-09-20 14:03:05 +0000

    Changed display to io:format

--- org.erlide.core/include/erlide.hrl
+++ org.erlide.core/include/erlide.hrl
@@ -1,7 +1,8 @@
 
 -ifdef(DEBUG).
 -compile(export_all).
--define(D(T), erlang:display({?MODULE, ?LINE, T})).
+%-define(D(T), erlang:display({?MODULE, ?LINE, T})).
+-define(D(T), io:format("~p\n", [{?MODULE, ?LINE, T}])).
 -else.
 -define(D(T), ok).
 -endif.
