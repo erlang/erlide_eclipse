dd46999df2a6499d2a27d980225f02aee1c3eb68
Jakob Cederlund <jakobce@gmail.com>
2007-10-25 13:05:28 +0000

    Removed warnings

--- org.erlide.ui/src/org/erlide/ui/actions/ToggleNatureAction.java
+++ org.erlide.ui/src/org/erlide/ui/actions/ToggleNatureAction.java
@@ -35,7 +35,7 @@ public class ToggleNatureAction implements IObjectActionDelegate {
 	 */
 	public void run(IAction action) {
 		if (fSelection instanceof IStructuredSelection) {
-			for (final Iterator it = ((IStructuredSelection) fSelection)
+			for (final Iterator<?> it = ((IStructuredSelection) fSelection)
 					.iterator(); it.hasNext();) {
 				final Object element = it.next();
 				IProject project = null;
