394ae637a1a936dc5ae491965842127a2d7402b4
Vlad Dumitrescu <vladdu55@gmail.com>
2007-11-02 18:46:21 +0000

    Moved remotely

--- org.erlide.build/build.bat
+++
@@ -1,7 +0,0 @@
-set ECLIPSE_HOME=%1
-set ERLANG_HOME=%2
-set WORKSPACE=%3
-
-java -jar %ECLIPSE_HOME%/startup.jar -application org.eclipse.ant.core.antRunner -Dbuilder=%WORKSPACE%/org.erlide.build -DlocalWorkspace=%WORKSPACE% -DerlRoot=%ERLANG_HOME% %4 %5 %6 %7 %8 %9
-
-
