c6907fb90b72332a77a3c97b45ce50b30b087fed
Tomas Daarstadt <daatomas@gmail.com>
2007-11-23 12:21:01 +0000

    added a refresh

--- org.erlide.ui/src/org/erlide/ui/navigator/ErlangFileContentProvider.java
+++ org.erlide.ui/src/org/erlide/ui/navigator/ErlangFileContentProvider.java
@@ -154,6 +154,7 @@ public class ErlangFileContentProvider implements ITreeContentProvider,
 		case IResource.ROOT:
 		case IResource.PROJECT:
 		case IResource.FOLDER:
+			viewer.refresh();
 			return true;
 		case IResource.FILE:
 			final IFile file = (IFile) source;
