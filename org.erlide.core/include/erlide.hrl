a06592bd0dc845a543503efee3a8819b1d7e55bc
Jakob Cederlund <jakobce@gmail.com>
2008-03-04 15:50:08 +0000

    Changed D macro in erlide.hrl to use erlide_log

--- org.erlide.core/include/erlide.hrl
+++ org.erlide.core/include/erlide.hrl
@@ -2,7 +2,8 @@
 -ifdef(DEBUG).
 -compile(export_all).
 %-define(D(T), erlang:display({?MODULE, ?LINE, T})).
--define(D(T), io:format("~p\n", [{?MODULE, ?LINE, T}])).
+%-define(D(T), io:format("~p\n", [{?MODULE, ?LINE, T}])).
+-define(D(T), erlide_log:erlangLog(?MODULE, ?LINE, debug, T)).
 -else.
 -define(D(T), ok).
 -endif.
