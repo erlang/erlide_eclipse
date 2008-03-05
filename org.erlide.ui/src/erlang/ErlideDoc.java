80a0b54b7936ea6357fa7893127e603b0adee737
Jakob Cederlund <jakobce@gmail.com>
2008-03-05 09:18:04 +0000

    Simple module completion proposal

--- org.erlide.ui/src/erlang/ErlideDoc.java
+++ org.erlide.ui/src/erlang/ErlideDoc.java
@@ -1,5 +1,7 @@
 package erlang;
 
+import java.util.List;
+
 import org.erlide.jinterface.rpc.RpcException;
 import org.erlide.runtime.backend.BackendManager;
 import org.erlide.runtime.backend.IBackend;
@@ -11,26 +13,33 @@ import com.ericsson.otp.erlang.OtpErlangObject;
 
 public class ErlideDoc {
 
-	public static  OtpErlangObject getExported(final IBackend b, String prefix,
+	public static OtpErlangObject getExported(final IBackend b, String prefix,
 			final String mod) throws ErlangRpcException, BackendException,
 			RpcException {
-		final OtpErlangObject res = b.rpcx("erlide_otp_doc",
-				"get_exported", "as", mod, prefix);
+		final OtpErlangObject res = b.rpcx("erlide_otp_doc", "get_exported",
+				"as", mod, prefix);
+		return res;
+	}
+
+	public static OtpErlangObject getModules(final IBackend b, String prefix,
+			List<String> projectModules) throws ErlangRpcException,
+			BackendException, RpcException {
+		final OtpErlangObject res = b.rpcx("erlide_otp_doc", "get_modules",
+				"sls", prefix, projectModules);
 		return res;
 	}
 
 	public static OtpErlangObject getFunDoc(OtpErlangList list, String mod,
 			final String s) throws ErlangRpcException, BackendException,
 			RpcException {
-		final OtpErlangObject r1 = BackendManager.getDefault()
-				.getIdeBackend().rpcx("erlide_otp_doc",
-						"get_doc_from_fun_arity_list", "axs", mod, list, s);
+		final OtpErlangObject r1 = BackendManager.getDefault().getIdeBackend()
+				.rpcx("erlide_otp_doc", "get_doc_from_fun_arity_list", "axs",
+						mod, list, s);
 		return r1;
 	}
 
 	@SuppressWarnings("boxing")
-	public
-	static OtpErlangObject getDocFromScan(int offset, String s,
+	public static OtpErlangObject getDocFromScan(int offset, String s,
 			String module, OtpErlangList imports) throws ErlangRpcException,
 			BackendException, RpcException {
 		OtpErlangObject r1;
