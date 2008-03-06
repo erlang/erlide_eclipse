6db4dcd6ededcfa6e86baf594fd5be1873308b0f
Jakob Cederlund <jakobce@gmail.com>
2008-03-06 10:56:57 +0000

    Some cleanup

--- org.erlide.ui/src/erlang/ErlideOpen.java
+++ org.erlide.ui/src/erlang/ErlideOpen.java
@@ -1,5 +1,7 @@
 package erlang;
 
+import java.util.List;
+
 import org.erlide.jinterface.rpc.RpcException;
 import org.erlide.runtime.backend.BackendManager;
 import org.erlide.runtime.backend.IBackend;
@@ -15,35 +17,32 @@ public class ErlideOpen {
 
 	public static String getIncludeLib(String s) throws ErlangRpcException,
 			BackendException, RpcException {
-		final IBackend b = BackendManager.getDefault()
-				.getIdeBackend();
-		final OtpErlangObject t = b.rpcx("erlide_open",
-				"get_include_lib", null, s);
+		final IBackend b = BackendManager.getDefault().getIdeBackend();
+		final OtpErlangObject t = b.rpcx("erlide_open", "get_include_lib",
+				null, s);
 		if (t instanceof OtpErlangTuple) {
-			final OtpErlangObject es = ((OtpErlangTuple) t)
-					.elementAt(1);
+			final OtpErlangObject es = ((OtpErlangTuple) t).elementAt(1);
 			s = ((OtpErlangString) es).stringValue();
 		}
 		return s;
 	}
 
 	public static OtpErlangObject getSourceFromModule(final IBackend b,
-			final OtpErlangList pathVars, final String mod,
+			final List<OtpErlangTuple> pathVars, final String mod,
 			String fExternalModules) throws ErlangRpcException,
 			BackendException, RpcException {
 		final OtpErlangObject res2 = b.rpcx("erlide_open",
-				"get_source_from_module", "asx", mod, fExternalModules,
+				"get_source_from_module", "aslx", mod, fExternalModules,
 				pathVars);
 		return res2;
 	}
 
 	@SuppressWarnings("boxing")
-	public
-	static OtpErlangObject getOpenInfo(final IBackend b,
-			int window, final OtpErlangList list,
-			final OtpErlangList pathVars, String fExternalModules)
-			throws ErlangRpcException, BackendException, RpcException {
-		final OtpErlangObject res = b.rpcx("erlide_open", "open_info", "xisx",
+	public static OtpErlangObject getOpenInfo(final IBackend b, int window,
+			final OtpErlangList list, final List<OtpErlangTuple> pathVars,
+			String fExternalModules) throws ErlangRpcException,
+			BackendException, RpcException {
+		final OtpErlangObject res = b.rpcx("erlide_open", "open_info", "xislx",
 				list, window, fExternalModules, pathVars);
 		return res;
 	}
