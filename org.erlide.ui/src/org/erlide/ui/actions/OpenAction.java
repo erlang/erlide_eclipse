515c64ac74c33a22f4fc3734451a8d8085037cd7
Jakob Cederlund <jakobce@gmail.com>
2008-04-22 16:59:03 +0000

    More in window (we should rewrite this to use erlang!)

--- org.erlide.ui/src/org/erlide/ui/actions/OpenAction.java
+++ org.erlide.ui/src/org/erlide/ui/actions/OpenAction.java
@@ -266,7 +266,7 @@ public class OpenAction extends SelectionDispatchAction {
 	@SuppressWarnings("boxing")
 	@Override
 	public void run() {
-		int window = 3;
+		int window = 150;
 
 		fEditor = (ErlangEditor) getSite().getPage().getActiveEditor();
 		final TokenWindow w = fEditor.getTokenWindow(window);
