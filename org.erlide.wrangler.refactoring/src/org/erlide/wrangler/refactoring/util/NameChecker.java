69fcd8960b078def4460a00cf01231c1fa011cd0
Vlad Dumitrescu <vladdu55@gmail.com>
2008-09-16 21:33:11 +0000

    fixed Java 1.6 artifact

--- org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/util/NameChecker.java
+++ org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/util/NameChecker.java
@@ -1,43 +1,42 @@
 package org.erlide.wrangler.refactoring.util;
 
-import com.ericsson.otp.erlang.OtpErlangAtom;
 
 public class NameChecker {
-	
-	private NameChecker() {}
-	
+
+	private NameChecker() {
+	}
+
 	public static boolean checkIsAtom(String s) {
 		if (s.length() == 0) {
-			return false;	
+			return false;
 		}
-		if(s.charAt(0) == '\'' && s.charAt(s.length()-1) == '\'') {
+		if (s.charAt(0) == '\'' && s.charAt(s.length() - 1) == '\'') {
 			return true;
-		}
-		else {
-			if (s.substring(0,1).replaceAll("[a-z]", "").isEmpty() && s.replaceAll("[A-Za-z_@0-9]", "").isEmpty()) {
+		} else {
+			if (s.substring(0, 1).replaceAll("[a-z]", "").length() == 0
+					&& s.replaceAll("[A-Za-z_@0-9]", "").length() == 0) {
 				return true;
 			} else {
 				return false;
 			}
 		}
 	}
-	
+
 	public static boolean checkIsVariable(String s) {
-		if(s.length() == 0) {
+		if (s.length() == 0) {
 			return false;
 		}
-		
-		if(s.startsWith("_") || s.substring(0,1).toUpperCase().equals(s.substring(0, 1)) ) {
-			if(s.replaceAll("[A-Za-z_@0-9]", "").isEmpty()) {
+
+		if (s.startsWith("_")
+				|| s.substring(0, 1).toUpperCase().equals(s.substring(0, 1))) {
+			if (s.replaceAll("[A-Za-z_@0-9]", "").length() == 0) {
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
 		} else {
 			return false;
 		}
 	}
-
 
 }
