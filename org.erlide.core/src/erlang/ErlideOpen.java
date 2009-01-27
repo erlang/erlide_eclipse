166938534b98e04a9fe7d25ce819367966cd41b2
Jakob Cederlund <jakobce@gmail.com>
2009-01-27 00:03:37 +0000

    http://www.assembla.com/spaces/erlide/tickets/236-Debugger---show-records-as-records-in-Variables-view

---
+++ org.erlide.core/src/erlang/ErlideOpen.java
@@ -0,0 +1,147 @@
+package erlang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.erlide.core.erlang.util.Util;
+import org.erlide.jinterface.rpc.RpcException;
+import org.erlide.jinterface.rpc.Tuple;
+import org.erlide.runtime.ErlLogger;
+import org.erlide.runtime.backend.Backend;
+import org.erlide.runtime.backend.exceptions.BackendException;
+import org.erlide.runtime.backend.exceptions.ErlangRpcException;
+
+import com.ericsson.otp.erlang.OtpErlangList;
+import com.ericsson.otp.erlang.OtpErlangObject;
+import com.ericsson.otp.erlang.OtpErlangString;
+import com.ericsson.otp.erlang.OtpErlangTuple;
+
+public class ErlideOpen {
+
+	public static String getIncludeLib(final Backend b, String s)
+			throws ErlangRpcException, BackendException, RpcException {
+		final OtpErlangObject t = b.rpcx("erlide_open", "get_include_lib", "s",
+				s);
+		if (t instanceof OtpErlangTuple) {
+			final OtpErlangObject es = ((OtpErlangTuple) t).elementAt(1);
+			s = ((OtpErlangString) es).stringValue();
+		}
+		return s;
+	}
+
+	public static OtpErlangObject getSourceFromModule(final Backend b,
+			final List<Tuple> pathVars, final String mod,
+			final String externalModules) throws ErlangRpcException,
+			BackendException, RpcException {
+		final OtpErlangObject res2 = b.rpcx("erlide_open",
+				"get_source_from_module", "aslx", mod, externalModules,
+				fixPathVars(pathVars));
+		return res2;
+	}
+
+	@SuppressWarnings("boxing")
+	public static OtpErlangObject getOpenInfo(final Backend b,
+			final int window, final OtpErlangList list,
+			final List<OtpErlangTuple> pathVars, final String fExternalModules)
+			throws ErlangRpcException, BackendException, RpcException {
+		final OtpErlangObject res = b.rpcx("erlide_open", "open_info", "xislx",
+				list, window, fExternalModules, pathVars);
+		return res;
+	}
+
+	private static List<OtpErlangTuple> fixPathVars(final List<Tuple> pathVars) {
+		final List<OtpErlangTuple> pathVars2 = new ArrayList<OtpErlangTuple>();
+		for (final Tuple t : pathVars) {
+			pathVars2.add(new OtpErlangTuple(new OtpErlangString((String) t
+					.get(0)), new OtpErlangString((String) t.get(1))));
+		}
+		return pathVars2;
+	}
+
+	@SuppressWarnings("boxing")
+	public static OpenResult open(final Backend b, final String scannerName,
+			final int offset, final String externalModules,
+			final List<Tuple> pathVars) throws RpcException, BackendException {
+		ErlLogger.debug("open offset " + offset);
+		final OtpErlangObject res = b.rpcx("erlide_open", "open", "aislx",
+				scannerName, offset, externalModules, fixPathVars(pathVars));
+		return new OpenResult(res);
+	}
+
+	public static OtpErlangTuple findFirstVar(final Backend b,
+			final String name, final String source) {
+		OtpErlangObject res;
+		try {
+			res = b.rpcx("erlide_open", "find_first_var", "ss", name, source);
+			if (res instanceof OtpErlangTuple) {
+				return (OtpErlangTuple) res;
+			}
+		} catch (final RpcException e) {
+			ErlLogger.warn(e);
+		} catch (final BackendException e) {
+			ErlLogger.warn(e);
+		}
+		return null;
+	}
+
+	public static List<String> getExternalModules(final Backend b,
+			final String prefix, final String externalModules,
+			final List<Tuple> pathVars) {
+		try {
+			final OtpErlangObject res = b.rpcx("erlide_open",
+					"get_external_modules", "sslx", prefix, externalModules,
+					fixPathVars(pathVars));
+			if (Util.isOk(res)) {
+				final OtpErlangTuple t = (OtpErlangTuple) res;
+				final OtpErlangList l = (OtpErlangList) t.elementAt(1);
+				final List<String> result = new ArrayList<String>(l.arity());
+				for (final OtpErlangObject i : l.elements()) {
+					result.add(Util.stringValue(i));
+				}
+				return result;
+			}
+		} catch (final RpcException e) {
+			ErlLogger.warn(e);
+		} catch (final BackendException e) {
+			ErlLogger.warn(e);
+		}
+		return new ArrayList<String>();
+	}
+
+	public static String getExternalInclude(final Backend b,
+			final String filePath, final String externalIncludes,
+			final List<Tuple> pathVars) {
+		try {
+			final OtpErlangObject res = b.rpcx("erlide_open",
+					"get_external_include", "sslx", filePath, externalIncludes,
+					fixPathVars(pathVars));
+			if (Util.isOk(res)) {
+				final OtpErlangTuple t = (OtpErlangTuple) res;
+				return Util.stringValue(t.elementAt(1));
+			}
+		} catch (final RpcException e) {
+			e.printStackTrace();
+		} catch (final BackendException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String getExternalModule(final Backend b, final String mod,
+			final String externalModules, final List<Tuple> pathVars) {
+		try {
+			final OtpErlangObject res = b.rpcx("erlide_open",
+					"get_external_module", "sslx", mod, externalModules,
+					fixPathVars(pathVars));
+			if (Util.isOk(res)) {
+				final OtpErlangTuple t = (OtpErlangTuple) res;
+				return Util.stringValue(t.elementAt(1));
+			}
+		} catch (final RpcException e) {
+			e.printStackTrace();
+		} catch (final BackendException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
---
+++ org.erlide.core/src/erlang/OpenResult.java
@@ -0,0 +1,138 @@
+package erlang;
+
+import org.erlide.core.util.ErlangFunction;
+import org.erlide.runtime.ErlLogger;
+
+import com.ericsson.otp.erlang.OtpErlangAtom;
+import com.ericsson.otp.erlang.OtpErlangLong;
+import com.ericsson.otp.erlang.OtpErlangObject;
+import com.ericsson.otp.erlang.OtpErlangString;
+import com.ericsson.otp.erlang.OtpErlangTuple;
+
+public class OpenResult {
+
+	private boolean isExternalCall = false;
+	private String name;
+	private String fun;
+	private int arity;
+	private String path;
+	private boolean isRecord = false;
+	private boolean isMacro = false;
+	private boolean isLocalCall = false;
+	private boolean isInclude = false;
+	private boolean isVariable = false;
+
+	public OpenResult(final OtpErlangObject res) {
+		if (!(res instanceof OtpErlangTuple)) {
+			return; // not a call, ignore
+		}
+		final OtpErlangTuple tres = (OtpErlangTuple) res;
+		final String kind = ((OtpErlangAtom) tres.elementAt(0)).atomValue();
+		try {
+			if (kind.equals("external")) {
+				isExternalCall = true;
+				name = ((OtpErlangAtom) tres.elementAt(1)).atomValue();
+				fun = ((OtpErlangAtom) tres.elementAt(2)).atomValue();
+				arity = ((OtpErlangLong) tres.elementAt(3)).intValue();
+				path = null;
+				if (tres.arity() > 4
+						&& tres.elementAt(4) instanceof OtpErlangString) {
+					path = ((OtpErlangString) tres.elementAt(4)).stringValue();
+				}
+			} else if (kind.equals("include")) {
+				isInclude = true;
+				final OtpErlangString s = (OtpErlangString) tres.elementAt(1);
+				name = s.stringValue();
+			} else if (kind.equals("local")) { // local call
+				isLocalCall = true;
+				fun = ((OtpErlangAtom) tres.elementAt(1)).atomValue();
+				arity = ((OtpErlangLong) tres.elementAt(2)).intValue();
+				// } else if (external.equals("variable")) {
+				// final OtpErlangTuple mf = (OtpErlangTuple) tres.elementAt(1);
+				// final OtpErlangAtom var = (OtpErlangAtom) mf.elementAt(0);
+			} else if (kind.equals("record") || kind.equals("macro")) {
+				isMacro = kind.equals("macro");
+				isRecord = kind.equals("record");
+				name = ((OtpErlangAtom) tres.elementAt(1)).atomValue();
+			} else if (kind.equals("variable")) {
+				isVariable = true;
+				final OtpErlangTuple t = (OtpErlangTuple) tres.elementAt(1);
+				final OtpErlangAtom a = (OtpErlangAtom) t.elementAt(0);
+				name = a.atomValue();
+			}
+		} catch (final Exception e) {
+			ErlLogger.warn(e);
+		}
+	}
+
+	public boolean isExternalCall() {
+		return isExternalCall;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFun() {
+		return fun;
+	}
+
+	public int getArity() {
+		return arity;
+	}
+
+	public ErlangFunction getFunction() {
+		return new ErlangFunction(fun, arity);
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public boolean isRecord() {
+		return isRecord;
+	}
+
+	public boolean isMacro() {
+		return isMacro;
+	}
+
+	public boolean isLocalCall() {
+		return isLocalCall;
+	}
+
+	public boolean isInclude() {
+		return isInclude;
+	}
+
+	public boolean isVariable() {
+		return isVariable;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final StringBuilder b = new StringBuilder("OpenResult {");
+		if (isRecord) {
+			b.append("record ").append(name);
+		} else if (isInclude) {
+			b.append("include \"").append(name).append("\"");
+		} else if (isMacro) {
+			b.append("macro ").append(name);
+		} else if (isRecord) {
+			b.append("record ").append(name);
+		} else if (isExternalCall) {
+			b.append("external ");
+			b.append(name).append(":").append(fun).append("/").append(arity);
+		} else if (isLocalCall) {
+			b.append("local ").append(fun).append("/").append(arity);
+		}
+		b.append("}");
+		return b.toString();
+	}
+
+}
