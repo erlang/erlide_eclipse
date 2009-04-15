6326e0af88e12f31c1489d4a45b4fcb9d66c1beb
Vlad Dumitrescu <vladdu55@gmail.com>
2009-04-15 08:44:01 +0000

    improved layout of ReportPreferencePage, hopefully better on OSX

--- org.erlide.ui/src/org/erlide/ui/prefs/ReportPreferencePage.java
+++ org.erlide.ui/src/org/erlide/ui/prefs/ReportPreferencePage.java
@@ -23,6 +23,8 @@ import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -35,8 +37,12 @@ import org.erlide.core.util.ErlideUtil;
 import org.erlide.runtime.ErlLogger;
 import org.erlide.ui.prefs.tickets.TicketInfo;
 
+import com.swtdesigner.SWTResourceManager;
+
 public class ReportPreferencePage extends PreferencePage implements
 		IWorkbenchPreferencePage {
+	public ReportPreferencePage() {
+	}
 
 	private Button sendButton;
 	private Label responseLabel_1;
@@ -47,65 +53,84 @@ public class ReportPreferencePage extends PreferencePage implements
 
 	private Label responseLabel;
 	Button attachTechnicalDataButton;
+	private Label label;
+	private Label lblDescription;
 
 	@Override
 	protected Control createContents(Composite parent) {
 		Composite panel = new Composite(parent, SWT.NONE);
-
+		panel.setLayout(new GridLayout(2, false));
 		final Label titleLabel = new Label(panel, SWT.NONE);
-		titleLabel.setText("Title");
-		titleLabel.setBounds(0, 5, 25, 15);
-
+		titleLabel.setAlignment(SWT.RIGHT);
+		{
+			GridData gridData = new GridData(SWT.RIGHT, SWT.CENTER, false,
+					false, 1, 1);
+			gridData.widthHint = 60;
+			titleLabel.setLayoutData(gridData);
+		}
+		titleLabel.setText("Title:");
 		this.ftitle = new Text(panel, SWT.BORDER);
-		this.ftitle.setBounds(46, 0, 416, 25);
+		ftitle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
+				1));
+
+		lblDescription = new Label(panel, SWT.NONE);
+		lblDescription.setAlignment(SWT.RIGHT);
+		lblDescription.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false,
+				false, 1, 1));
+		lblDescription.setText("Description:");
 
 		this.fbody = new Text(panel, SWT.V_SCROLL | SWT.MULTI | SWT.BORDER
 				| SWT.WRAP);
-		this.fbody.setBounds(46, 31, 416, 188);
-
+		{
+			GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false,
+					1, 1);
+			gridData.heightHint = 150;
+			fbody.setLayoutData(gridData);
+		}
 		this.fbody
 				.setText("(enter error description here, paste any relevant code too)");
-
+		final Label contactoptionalLabel = new Label(panel, SWT.NONE);
+		contactoptionalLabel.setAlignment(SWT.RIGHT);
+		contactoptionalLabel.setText("Contact (email):");
 		this.fcontact = new Text(panel, SWT.BORDER);
+		fcontact.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
 		fcontact.setText(System.getProperty("user.name"));
-		this.fcontact.setBounds(152, 225, 310, 25);
-
 		attachTechnicalDataButton = new Button(panel, SWT.CHECK);
+		attachTechnicalDataButton.setLayoutData(new GridData(SWT.CENTER,
+				SWT.CENTER, false, false, 2, 1));
 		attachTechnicalDataButton.setSelection(true);
 		attachTechnicalDataButton
-				.setText("Attach technical data (eclipse and erlide logs)");
-		attachTechnicalDataButton.setBounds(46, 256, 416, 20);
-
+				.setText("   Attach technical data (eclipse and erlide logs)   ");
 		sendButton = new Button(panel, SWT.NONE);
+		sendButton.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false,
+				false, 2, 1));
 		sendButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
 				postReport();
 			}
 		});
-		sendButton.setText("Send!");
-		sendButton.setBounds(205, 284, 62, 25);
-
-		final Label contactoptionalLabel = new Label(panel, SWT.NONE);
-		contactoptionalLabel.setText("Contact email (optional)");
-		contactoptionalLabel.setBounds(46, 228, 100, 15);
-
+		sendButton.setText("   Send!   ");
 		responseLabel = new Label(panel, SWT.CENTER);
 		responseLabel.setVisible(false);
+		responseLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false,
+				false, 2, 1));
 		responseLabel
 				.setText("The report was saved in the location below, you can now close this window.");
-		responseLabel.setBounds(47, 315, 415, 15);
-
 		locationLabel = new Text(panel, SWT.CENTER | SWT.WRAP | SWT.READ_ONLY);
+		locationLabel.setVisible(false);
+		locationLabel.setForeground(SWTResourceManager
+				.getColor(SWT.COLOR_DARK_BLUE));
+		locationLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 2, 1));
 		locationLabel.setBackground(Display.getCurrent().getSystemColor(
 				SWT.COLOR_WIDGET_BACKGROUND));
 		locationLabel.setText("location");
-		locationLabel.setBounds(46, 336, 415, 23);
-		locationLabel.setVisible(false);
-
 		responseLabel_1 = new Label(panel, SWT.CENTER);
-		responseLabel_1.setBounds(47, 364, 415, 15);
 		responseLabel_1.setVisible(false);
+		responseLabel_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 2, 1));
 		responseLabel_1
 				.setText("Please attach the report if you are writing a trouble ticket.");
 
