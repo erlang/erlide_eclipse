5b05614154f451bc55c6e4e727249c47e9e2c12c
Vlad Dumitrescu <vladdu55@gmail.com>
2009-04-16 08:39:17 +0000

    work on automated builds

--- org.erlide.build/build-erlide.bat
+++
@@ -1,4 +0,0 @@
-SET eclipseDir=c:\users\vlad\applications\eclipse34_clean\eclipse
-SET eclipseLauncher=%eclipseDir%\plugins\org.eclipse.equinox.launcher_1.0.101.R34x_v20081125.jar
-"c:\program files\java\jdk1.5.0_14\bin\java" -jar %eclipseLauncher% -application org.eclipse.ant.core.antRunner -buildfile build-erlide.xml 2>&1 1>build.log
-
