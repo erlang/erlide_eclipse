8f3d81528fd95f7e03017468e49a4810d9a4beb0
Vlad Dumitrescu <vladdu55@gmail.com>
2009-05-08 11:53:01 +0000

    moved tests to the right places

--- org.erlide.jinterface.tests/src/org/erlide/testing/JavaTests.java
+++ org.erlide.jinterface.tests/src/org/erlide/testing/JavaTests.java
@@ -4,17 +4,20 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Vlad Dumitrescu
  *******************************************************************************/
 package org.erlide.testing;
 
+import org.erlide.testing.java.JInterfaceTest;
+import org.erlide.testing.java.PatternMatchTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { JavaTests.class, ErlangTests.class })
-public class AllTests {
+@SuiteClasses( { PatternMatchTest.class, JInterfaceTest.class })
+public class JavaTests {
 
 }
