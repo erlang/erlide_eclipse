abfcd778a0660adbdf3af7d3ce86844f7edd48cc
Vlad Dumitrescu <vladdu55@gmail.com>
2009-05-13 10:24:02 +0000

    fixed license

--- org.erlide.core/src/org/erlide/core/erlang/util/BackendUtils.java
+++ org.erlide.core/src/org/erlide/core/erlang/util/BackendUtils.java
@@ -1,9 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2008 Vlad Dumitrescu and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
- * which accompanies this distribution.
- * 
+ * Copyright (c) 2009 Vlad Dumitrescu and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
  *     Vlad Dumitrescu
  *******************************************************************************/
