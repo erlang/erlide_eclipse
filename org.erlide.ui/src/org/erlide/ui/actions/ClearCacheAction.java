7a3527ad50a0df76540a5c33e608ea04e8358980
Jakob Cederlund <jakobce@gmail.com>
2009-05-14 13:45:30 +0000

    http://www.assembla.com/spaces/erlide/tickets/369-command-to-reset-scanner-and-parser-caches

--- org.erlide.ui/src/org/erlide/ui/actions/ClearCacheAction.java
+++ org.erlide.ui/src/org/erlide/ui/actions/ClearCacheAction.java
@@ -3,9 +3,13 @@
  */
 package org.erlide.ui.actions;
 
+import java.io.File;
 import java.util.ResourceBundle;
 
 import org.eclipse.ui.texteditor.TextEditorAction;
+import org.erlide.core.ErlangPlugin;
+import org.erlide.core.erlang.ErlScanner;
+import org.erlide.core.erlang.IErlModule;
 import org.erlide.ui.editors.erl.ErlangEditor;
 
 /**
@@ -15,6 +19,9 @@ import org.erlide.ui.editors.erl.ErlangEditor;
 public class ClearCacheAction extends TextEditorAction {
 
 	private final ErlangEditor erlangEditor;
+	private final String NOPARSE_CACHE_SUFFIX = ".noparse";
+	private final String SCANNER_CACHE_SUFFIX = ".scan";
+	private String suffixes[] = {NOPARSE_CACHE_SUFFIX, SCANNER_CACHE_SUFFIX};
 
 	public ClearCacheAction(final ResourceBundle bundle, final String prefix,
 			final ErlangEditor erlangEditor) {
@@ -29,6 +36,15 @@ public class ClearCacheAction extends TextEditorAction {
 	 */
 	@Override
 	public void run() {
+		final IErlModule module = erlangEditor.getModule();
+		for (String suffix : suffixes) {
+			final String cacheFileOSPath = ErlangPlugin.getDefault()
+			.getStateLocation().append(
+					ErlScanner.createScannerModuleName(module)
+					+ suffix).toOSString();
+			final File cacheFile = new File(cacheFileOSPath);
+			cacheFile.delete();
+		}
 		erlangEditor.resetParser();
 	}
 }
