88c14607b901a9674c7b8bf56489e439404e1237
Jakob Cederlund <jakobce@gmail.com>
2009-08-12 12:15:08 +0000

    https://www.assembla.com/spaces/erlide/tickets/402-overriden--int--module-lacks-functionality-of-the-original-one

--- org.erlide.core/erl_debugger/int.erl
+++ org.erlide.core/erl_debugger/int.erl
@@ -14,6 +14,17 @@
 %%
 %% Exported Functions
 %%
+
+-export([i/1, ni/1, n/1, nn/1, interpreted/0, file/1,
+	 interpretable/1, auto_attach/0, auto_attach/1, auto_attach/2,
+	 stack_trace/0, stack_trace/1, break/2, delete_break/2, break_in/3,
+	 del_break_in/3, no_break/0, no_break/1, disable_break/2, enable_break/2,
+	 action_at_break/3, test_at_break/3, get_binding/2,
+	 all_breaks/0, all_breaks/1, snapshot/0, clear/0, continue/1, continue/3]).
+
+%%
+%% Internal Export
+%%
 -export([eval/3]).
 
 %%
@@ -26,4 +37,39 @@ eval(Mod, Func, Args) ->
 %%
 %% Local Functions
 %%
+
+i(M) -> erlide_int:i(M).
+ni(M) -> erlide_int:ni(M).
+n(M) -> erlide_int:n(M).
+nn(M) -> erlide_int:nn(M).
+interpreted() -> erlide_int:interpreted().
+file(M) -> erlide_int:file(M).
+interpretable(M) -> erlide_int:interpretable(M).
+auto_attach() -> nyi().
+auto_attach(_) -> nyi().
+auto_attach(_, _) ->nyi().
+stack_trace() -> nyi().
+stack_trace(_) -> nyi().
+break(Mod, Line) -> erlide_int:break(Mod, Line).
+delete_break(Mod, Line) -> erlide_int:delete_break(Mod, Line).
+break_in(Mod, Func, Arity) -> erlide_int:break_in(Mod, Func, Arity).
+del_break_in(Mod, Func, Arity) -> erlide_int:del_break_in(Mod, Func, Arity).
+no_break() -> erlide_int:no_break().
+no_break(Mod) -> erlide_int:no_break(Mod).
+action_at_break(_, _, _) -> nyi().
+test_at_break(_, _, _) -> nyi().
+get_binding(_Var, _Bs) -> nyi().
+all_breaks() -> erlide_int:all_breaks().
+all_breaks(Mod) -> erlide_int:all_breaks(Mod).
+disable_break(Mod, Line) -> erlide_int:disable_break(Mod, Line).
+enable_break(Mod, Line) -> erlide_int:enable_break(Mod, Line).
+snapshot() -> nyi().
+clear() -> nyi().
+continue(Pid) -> erlide_int:continue(Pid).
+continue(X, Y, Z) -> erlide_int:continue(X, Y, Z).
+
+nyi() -> 
+    io:format("unavailable from within ErlIDE, use Eclipse debugger functions instead\n"), 
+    {error, unavaible}.
+
 
