a2be102045a01e802ba169472d690305fed3e616
Vlad Dumitrescu <vladdu55@gmail.com>
2009-09-11 11:12:16 +0000

    erlide_monitor interval should be 5 minutes

--- org.erlide.core/erl/erlide_monitor.erl
+++ org.erlide.core/erl/erlide_monitor.erl
@@ -28,7 +28,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {
-				poll_interval= 30000,
+				poll_interval= 300000,
 				subscribers=[],
 				ignored_processes=[],
 				ignored_ets=[],
