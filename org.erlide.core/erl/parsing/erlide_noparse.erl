70976296bb1cc067086af83ef781cf63c2eb419d
Vlad Dumitrescu <vladdu55@gmail.com>
2009-10-05 12:12:26 +0000

    split noparse server functionality into a separate module

--- org.erlide.core/erl/parsing/erlide_noparse.erl
+++ org.erlide.core/erl/parsing/erlide_noparse.erl
@@ -1,6 +1,6 @@
 %% Author: jakob
 %% Created: Mar 23, 2006
-%% Description: TODO: Add description to erlide_open
+%% Description: TODO: Add description to erlide_noparse
 
 -module(erlide_noparse).
 
@@ -10,14 +10,7 @@
 
 -export([initial_parse/6, reparse/1]).
 
-%% server stuff, currently only for testing
--export([modules/0, dump_module/1, dump_log/0, create/3, destroy/1, logging/1, all/0, stop/0, find/3,
-         xdump/0]).
-
 -compile(export_all).
-
-%% internal exports 
--export([loop/1]).
 
 %%
 %% Include files
@@ -34,44 +27,6 @@
 %%
 %% API Functions
 %%
-
-modules() ->
-    server_cmd(modules, []).
-
-create(Module, Model, ErlidePath) when is_atom(Module), is_list(ErlidePath) ->
-    server_cmd(create, {Module, Model, ErlidePath}).
-
-destroy(Module) when is_atom(Module) ->
-    server_cmd(destroy, Module).
-
-dump_log() ->
-    server_cmd(dump_log, []).
-
-logging(on) ->
-    server_cmd(logging, on);
-logging(off) ->
-    server_cmd(logging, off).
-
-all() ->
-    server_cmd(all, []).
-
-xdump() ->
-    server_cmd(xdump, []).
-
-%% cdump() ->
-%%     server_cmd(cdump, []).
-
-%% vdump() ->
-%%     server_cmd(vdump, []).
-
-dump_module(Module) when is_atom(Module) ->
-    server_cmd(dump_module, Module).
-
-stop() ->
-    server_cmd(stop, []).
-
-find(M, F, A) ->
-    server_cmd(find, {external_call, {M, F, A}}).
 
 initial_parse(ScannerName, ModuleFileName, InitialText, StateDir, ErlidePath, UpdateCaches) ->
     try
@@ -90,7 +45,7 @@ initial_parse(ScannerName, ModuleFileName, InitialText, StateDir, ErlidePath, Up
         {Cached, Res} = erlide_util:check_and_renew_cached(ModuleFileName, CacheFileName,
 							   ?CACHE_VERSION, RenewFun, CacheFun,
 							   UpdateCaches),
-        update_state(ScannerName, Res),
+        erlide_noparse_server:update_state(ScannerName, Res),
 	?D(updated),
         {ok, Res, Cached}
     catch
@@ -101,7 +56,7 @@ initial_parse(ScannerName, ModuleFileName, InitialText, StateDir, ErlidePath, Up
 reparse(ScannerName) ->
     try
         Res = do_parse(ScannerName, "", "", "", "", false),
-        update_state(ScannerName, Res),
+        erlide_noparse_server:update_state(ScannerName, Res),
         {ok, Res, unused}
     catch
         error:Reason ->
@@ -136,7 +91,7 @@ do_parse(ScannerName, ModuleFileName, InitalText, StateDir, ErlidePath, UpdateCa
     ?D(length(Collected)),
     CommentedCollected = get_function_comments(Collected, Comments),
     Model = #model{forms=CommentedCollected, comments=Comments},
-    create(ScannerName, Model, ErlidePath),
+    erlide_noparse_server:create(ScannerName, Model, ErlidePath),
     Model.
 
 parse_test(ScannerName, File) ->
@@ -146,7 +101,7 @@ parse_test(ScannerName, File) ->
     Functions = split_after_dots(UncommentToks, [], []),
     Collected = [classify_and_collect(I) || I <- Functions, I =/= [eof]],
     Model = #model{forms=Collected, comments=Comments},
-    create(ScannerName, Model, ""),
+    erlide_noparse_server:create(ScannerName, Model, ""),
     ok.
 
 classify_and_collect(C) ->
@@ -505,103 +460,6 @@ extract_comments([C = #token{kind=comment, line=N} | Rest], _, TAcc, CAcc) ->
 extract_comments([T | Rest], _, TAcc, CAcc) ->
     extract_comments(Rest, -1, [T | TAcc], CAcc).
 
-%%
-%% Server
-%%
-
-update_state(ScannerName, Model) ->
-    server_cmd(update_state, {ScannerName, Model}).
-
-server_cmd(Command, Args) ->
-    spawn_server(),
-    ?SERVER ! {Command, self(), Args},
-    receive
-	{Command, _Pid, Result} ->
-	    Result
-    end.
-
-spawn_server() ->
-    case whereis(?SERVER) of
-        undefined ->
-			Pid = spawn(fun() ->
-								?SAVE_CALLS,
-								loop([]) 
-						end),
-			erlang:register(?SERVER, Pid);
-        _ ->
-            ok
-    end.
-
-loop(Modules) ->
-    receive
-        {stop, From, []} ->
-            reply(stop, From, stopped);
-        {Cmd, From, Args} ->
-            NewMods = cmd(Cmd, From, Args, Modules),
-            ?MODULE:loop(NewMods)
-    end.
-
-cmd(Cmd, From, Args, Modules) ->
-    try
-        case get(logging) of
-            on ->
-                put(log, get(log)++[{Cmd, Args}]);
-            _ ->
-                ok
-        end,
-        case do_cmd(Cmd, Args, Modules) of
-            {R, NewMods} ->
-                reply(Cmd, From, R),
-                NewMods;
-            NewMods ->
-                reply(Cmd, From, ok),
-                NewMods
-        end
-    catch
-        exit:Error ->
-            reply(Cmd, From, {exit, Error}),
-            Modules;
-        error:Error ->
-            reply(Cmd, From, {error, Error}),
-            Modules
-    end.
-
-reply(Cmd, From, R) ->
-	From ! {Cmd, self(), R}.
-
-do_cmd(create, {Mod, Model, ErlidePath}, Modules) ->
-    [#module{name=Mod, model=Model, erlide_path=ErlidePath} | lists:keydelete(Mod, #module.name, Modules)];
-do_cmd(destroy, Mod, Modules) ->
-    lists:keydelete(Mod, #module.name, Modules);
-do_cmd(update_state, {Mod, Model}, Modules) ->
-    NewMod = case lists:keysearch(Mod, #module.name, Modules) of
-                 {value, OldMod} ->
-                     OldMod#module{model=Model};
-                 false ->
-                     #module{name=Mod, model=Model}
-             end,
-    [NewMod | lists:keydelete(Mod, #module.name, Modules)];
-do_cmd(all, [], Modules) ->
-    {Modules, Modules};
-do_cmd(modules, [], Modules) ->
-    Mods = [{Name, Path} || #module{name=Name, erlide_path=Path} <- Modules],
-    {Mods, Modules};
-do_cmd(dump_module, Mod, Modules) ->
-    {value, Module} = lists:keysearch(Mod, #module.name, Modules),
-    {Module, Modules};
-do_cmd(logging, OnOff, Modules) ->
-    put(log, []),
-    {put(logging, OnOff), Modules};
-do_cmd(find, {external_call, {M, F, A}}, Modules) ->
-    {find_external_call({M, F, A}, Modules), Modules};
-do_cmd(dump_log, [], Modules) ->
-    {get(log), Modules};
-%% do_cmd(cdump, [], Modules) ->
-%%     {do_cdump(Modules), Modules};
-%% do_cmd(vdump, [], Modules) ->
-%%     {do_vdump(Modules), Modules};
-do_cmd(xdump, [], Modules) ->
-    {do_xdump(Modules), Modules}.
 
 %% find all external calls in the modules known
 find_external_call(MFA, Modules) ->
