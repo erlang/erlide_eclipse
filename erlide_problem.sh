5f7829d4969a5749a4be8ba65c1662ebaac98833
Vlad Dumitrescu <vladdu55@gmail.com>
2009-10-23 08:50:38 +0000

    added a script to create a problem report

---
+++ erlide_problem.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/sh
+
+# run this script to gather the logs from an erlide session
+
+# erlide_problem [-o <output_dir>] [<workspace_dir>]
+# default output_dir is current dir
+# default workspace_dir is current dir
+
+##### hmmm, bash or tcsh???
+
+echo Not implemented yet...
+
