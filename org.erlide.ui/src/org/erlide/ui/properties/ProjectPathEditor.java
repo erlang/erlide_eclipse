6e44a38cbeb1af8668003dc68cf6766af763027c
Vlad Dumitrescu <vladdu55@gmail.com>
2010-01-22 14:20:43 +0100

    better ProjectPathEditor

--- org.erlide.ui/src/org/erlide/ui/properties/ProjectPathEditor.java
+++ org.erlide.ui/src/org/erlide/ui/properties/ProjectPathEditor.java
@@ -1,19 +1,20 @@
 package org.erlide.ui.properties;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.preference.PathEditor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 
 public class ProjectPathEditor extends PathEditor {
 	private final String dirChooserLabelText;
-	private final String rootPath;
+	private final IProject project;
 
 	public ProjectPathEditor(final String name, final String labelText,
-			final String dirChooserLabelText, final String rootPath,
+			final String dirChooserLabelText, final IProject project,
 			final Composite parent) {
 		init(name, labelText);
 		this.dirChooserLabelText = dirChooserLabelText;
-		this.rootPath = rootPath;
+		this.project = project;
 		createControl(parent);
 	}
 
@@ -24,7 +25,7 @@ public class ProjectPathEditor extends PathEditor {
 		if (dirChooserLabelText != null) {
 			dialog.setMessage(dirChooserLabelText);
 		}
-		dialog.setFilterPath(rootPath);
+		dialog.setFilterPath(project.getLocation().toString());
 
 		String dir = dialog.open();
 		if (dir != null) {
