522bbb835b4fb7d6521edb19ea6905484fe96237
Vlad Dumitrescu <vladdu55@gmail.com>
2010-03-01 11:23:29 +0100

    moved nightly builds on sf frs

--- org.erlide.update/publish.sh
+++ org.erlide.update/publish.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-scp -r features/ ${1}@shell.sourceforge.net:/home/groups/e/er/erlide/htdocs/update
-scp -r plugins/ ${1}@shell.sourceforge.net:/home/groups/e/er/erlide/htdocs/update
-scp web/site* ${1}@shell.sourceforge.net:/home/groups/e/er/erlide/htdocs/update/web
-scp index.html ${1}@shell.sourceforge.net:/home/groups/e/er/erlide/htdocs/update
-scp site.xml ${1}@shell.sourceforge.net:/home/groups/e/er/erlide/htdocs/update
+scp -r features/ ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
+scp -r plugins/ ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
+scp web/site* ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update/web
+scp index.html ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
+scp site.xml ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
