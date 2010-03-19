54bc4d0a01b0a994d5d9bd652a9c455c7b45cc14
Jakob C <jakobce@gmail.com>
2010-03-19 15:47:07 +0100

    wip

--- org.erlide.kernel.ide/src/erlide_search_server.erl
+++ org.erlide.kernel.ide/src/erlide_search_server.erl
@@ -38,7 +38,7 @@
 
 -define(SERVER, erlide_search_server).
 -record(state, {modules=[], refs=[]}). %% FIXME overly simple data model
--record(ref, {module, line, kind, data, function, clause, has_clauses}).
+-record(ref, {module_no, line, data, function, clause, has_clauses}).
 -record(module, {path, mtime}).
 
 %%
@@ -164,19 +164,23 @@ do_cmd(find_refs, Ref, State) ->
 do_cmd(state, _, State) ->
     {State, State}.
 
-do_find_refs(Data, State) ->
-    Found = find_data(State#state.refs, Data, []),
+do_find_refs(Data, #state{refs=Refs, modules=Modules} = State) ->
+    Found = find_data(Refs, Data, Modules, []),
     {{ok, Found}, State}.
 
-find_data([], _, Acc) ->
+get_module(ModN, Modules) ->
+    {value, {ModN, Module}} = lists:keysearch(ModN, 1, Modules),
+    Module.
+
+find_data([], _, _, Acc) ->
     Acc;
-find_data([#ref{module=M, line=L, function=F, clause=C, data=D, has_clauses=H} | Rest], 
-          Data, Acc) ->
+find_data([#ref{module_no=ModN, line=L, function=F, clause=C, data=D, has_clauses=H} | Rest], 
+          Data, Modules, Acc) ->
     case D of
         Data ->
-            find_data(Rest, Data, [{M, L-1, F, format_clause(C), H} | Acc]);
+            find_data(Rest, Data, Modules, [{get_module(ModN, Modules), L-1, F, format_clause(C), H} | Acc]);
         _ ->
-            find_data(Rest, Data, Acc)
+            find_data(Rest, Data, Modules, Acc)
     end.
 
 format_clause([], Acc) ->
@@ -191,17 +195,30 @@ format_clause([_|_] = Args) ->
 format_clause(Else) ->
     Else.
 
+get_max_module_no([], N) ->
+    N;
+get_max_module_no([{I, _Module} | Rest], N) when N < I ->
+    get_max_module_no(Rest, I);
+get_max_module_no([_ | Rest], N) ->
+    get_max_module_no(Rest, N).
+
 do_add_modules([], State) ->
     ?D(State),
     State;
 do_add_modules([Filename | Rest], State = #state{refs = Refs, modules = Modules}) ->
     ?D(Filename),
-    {Module, MoreRefs} = module_refs(Filename),
-    ?D({Module, MoreRefs}),
-    do_add_modules(Rest, State#state{refs = MoreRefs ++ Refs, modules = [Module | Modules]}).
+    N = get_max_module_no(Modules, 0)+1,
+    case module_refs(Filename, Modules, N) of
+        {Module, MoreRefs} ->
+            ?D({Module, MoreRefs}),
+            do_add_modules(Rest, State#state{refs = MoreRefs ++ Refs,
+                                             modules = [{N, Module} | Modules]});
+        false ->
+            do_add_modules(Rest, State)
+    end.
 
-get_node_kind_and_line_no({tree, Kind, {attr, Line, _, _}, _}) ->
-    {Kind, Line}.
+get_node_line_no({tree, _Kind, {attr, Line, _, _}, _}) ->
+    Line.
 
 node_to_data({tree, application, _, _} = Node) ->
     case erl_syntax_lib:analyze_application(Node) of
@@ -224,10 +241,10 @@ node_to_data({tree, clause, _, {clause, Args, _, _Body}}) ->
 %%       {attr,10,[],none},
 %%       {attribute,{atom,10,include},[{string,10,"b.hrl"}]}}
 node_to_data({tree, attribute, _, _} = Node) ->
-    ?D(Node),
     {_, _, _, Attr} = Node,
     case Attr of
-        {attribute, {atom, _, include}, Value} ->
+        {attribute, {atom, _, Include}, Value} 
+          when Include=:=include; Include=:=include_lib ->
             case Value of
                 [{string, _, S}] ->
                     {include, S};
@@ -240,28 +257,25 @@ node_to_data({tree, attribute, _, _} = Node) ->
 node_to_data(_) ->
     false.
 
-node_to_ref(Module, Node, {CurFunc, CurClause, HasClauses, Refs} = Acc) ->
+node_to_ref(ModN, Node, {CurFunc, CurClause, HasClauses, Refs} = Acc) ->
     case node_to_data(Node) of
         false ->
             Acc;
         {function, F} ->
             {_, _, _, Func} = Node,
             {_, _, Clauses} = Func,
-            ?D(Clauses),
             {F, CurClause, length(Clauses) > 1, Refs};
         {clause, C} ->
-            ?D(C),
             {CurFunc, C, HasClauses, Refs};
         Data ->
-            ?D(Data),
             CurFuncOrAttr = case Data of
                                 {include, S} ->
                                     "-include(\""++S++"\")";
                                 _ ->
                                     CurFunc
                             end,
-            {Kind, Line} = get_node_kind_and_line_no(Node),
-            Ref = #ref{module=Module, line=Line, kind=Kind, 
+            Line = get_node_line_no(Node),
+            Ref = #ref{module_no=ModN, line=Line, 
                        data=Data, function=CurFuncOrAttr, clause=CurClause,
                        has_clauses=HasClauses},
             {CurFunc, CurClause, HasClauses, [Ref | Refs]}
@@ -280,15 +294,35 @@ fold_nodes(F, Acc0, Node) when is_tuple(Node) ->
             fold_nodes(F, Acc1, Gs)
     end.
 
-module_refs(Filename) when is_list(Filename) ->
-    {ok, FileInfo} = file:read_file_info(Filename),
-    {ok, Forms} = epp_dodger:parse_file(Filename),
-    Module = #module{path=Filename, mtime=FileInfo#file_info.mtime},
-    {_, _, _, MoreRefs} = fold_nodes(fun(Node, Acc) ->
-                                          node_to_ref(Module, Node, Acc) 
-                                  end, {none, none, false, []}, Forms),
-    ?D(MoreRefs),
-    {Module, MoreRefs}.
+should_update_module([], _Filename) ->
+    true;
+should_update_module([{_N, #module{path=Path, mtime=Mtime}} | Rest], Filename) ->
+    ?D({Path, Filename}),
+    case Path of
+        Filename ->
+            {ok, FileInfo} = file:read_file_info(Filename),
+            Mtime =/= FileInfo#file_info.mtime;
+        _ ->
+            should_update_module(Rest, Filename)
+    end.
+%% should_update_module([_ | Rest], Filename) ->
+%%     should_update_module(Rest, Filename).
+
+module_refs(Filename, Modules, N) when is_list(Filename) ->
+    ?D({Filename, N}),
+    case should_update_module(Modules, Filename) of
+        true ->
+            {ok, FileInfo} = file:read_file_info(Filename),
+            {ok, Forms} = epp_dodger:parse_file(Filename),
+            Module = #module{path=Filename, mtime=FileInfo#file_info.mtime},
+            {_, _, _, MoreRefs} = fold_nodes(fun(Node, Acc) ->
+                                                     node_to_ref(N, Node, Acc) 
+                                             end, {none, none, false, []}, Forms),
+            ?D(MoreRefs),
+            {Module, MoreRefs};
+        false ->
+            false
+    end.
 
 
 
