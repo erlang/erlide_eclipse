1a95b3546b8ca746544dadf945138ba01d513223
Orosz György <G.Orosz@kent.ac.uk>
2010-04-08 14:47:35 +0200

    Core wrangler is separated.

--- org.erlide.wrangler.core/wrangler/src/wrangler.erl
+++ org.erlide.wrangler.core/wrangler/src/wrangler.erl
@@ -88,7 +88,8 @@
          merge_let_eclipse/3, merge_let_1_eclipse/4,
 	 eqc_statem_to_record_eclipse/3,eqc_statem_to_record_1_eclipse/7,
 	 eqc_fsm_to_record_eclipse/3,eqc_fsm_to_record_1_eclipse/7,
-	 gen_fsm_to_record_eclipse/3,gen_fsm_to_record_1_eclipse/7
+	 gen_fsm_to_record_eclipse/3,gen_fsm_to_record_1_eclipse/7,
+	 init_eclipse/0
 	]).
 
 -export([try_refactoring/3, try_inspector/3]).
@@ -1151,6 +1152,8 @@ gen_fsm_to_record_1_eclipse(FileName, RecordName, RecordFields, StateFuns, IsTup
     try_refactoring(refac_state_to_record, gen_fsm_to_record_1_eclipse, 
 		    [FileName, RecordName, RecordFields, StateFuns, IsTuple, SearchPaths, TabWidth]).
   
+init_eclipse()->
+	application:start(wrangler_app).
 
 %@private
 eqc_statem_to_fsm(FileName, StateName, SearchPaths, TabWidth) ->
@@ -1163,15 +1166,15 @@ eqc_statem_to_fsm_eclipse(FileName, StateName, SearchPaths, TabWidth) ->
 
 %%@private
 try_to_apply(Mod, Fun, Args, Msg) -> 
-%%    try 
-	apply(Mod, Fun, Args).
-%%     catch
-%%	 throw:Error -> 
-%%	     Error;    %% wrangler always throws Error in the format of '{error, string()}';
-%%	 E1:E2->
-  %%   	     refac_io:format("E1E2:\n~p\n", [{E1, E2}]),
-%%	     {error, Msg}
-  %%   end.
+    try 
+	apply(Mod, Fun, Args)
+     catch
+	 throw:Error -> 
+	     Error;    %% wrangler always throws Error in the format of '{error, string()}';
+	 E1:E2->
+     	     refac_io:format("E1E2:\n~p\n", [{E1, E2}]),
+	     {error, Msg}
+     end.
 
 %%@private
 try_refactoring(Mod, Fun, Args) ->
