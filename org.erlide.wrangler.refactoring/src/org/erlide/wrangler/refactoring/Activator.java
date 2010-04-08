6d64ae3c9e19d8ecda6508915f177f4df928bdfa
Orosz György <G.Orosz@kent.ac.uk>
2010-04-08 16:37:45 +0200

    Temporary solution for starting wrangler_app.

--- org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/Activator.java
+++ org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/Activator.java
@@ -2,7 +2,12 @@ package org.erlide.wrangler.refactoring;
 
 import java.io.IOException;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.erlide.core.erlang.ErlangCore;
+import org.erlide.jinterface.rpc.RpcResult;
+import org.erlide.jinterface.util.ErlLogger;
+import org.erlide.runtime.backend.ErlideBackend;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -39,42 +44,63 @@ public class Activator extends AbstractUIPlugin {
 	 * @throws CoreException
 	 *             detailed exception about the loading process errors
 	 */
-	/*
-	 * private void initWrangler() throws CoreException { try { Path pluginPath
-	 * = getPluginPath(); IPath wranglerRootPath =
-	 * pluginPath.append("wrangler"); String wranglerEbinPath =
-	 * wranglerRootPath.append("ebin") .toOSString(); String wranglerAppPath =
-	 * wranglerRootPath.append("app") .toOSString();
-	 * 
-	 * String wranglerSrcPath = wranglerRootPath.append("erl") .toOSString();
-	 * 
-	 * ErlLogger .debug("Wrangler beam files found at: " + wranglerEbinPath);
-	 * 
-	 * ErlideBackend mb = ErlangCore.getBackendManager().getIdeBackend();
-	 * 
-	 * ErlLogger.debug("Managed backend found:" + mb.getJavaNodeName());
-	 * 
-	 * ErlangCode.addPathA(mb, wranglerEbinPath); ErlangCode.addPathA(mb,
-	 * wranglerSrcPath); ErlangCode.addPathA(mb, wranglerAppPath);
-	 * ErlLogger.debug("Wrangler path has been added.");
-	 * 
-	 * RpcResult res = mb.call_noexception("code", "load_file", "a",
-	 * "wrangler"); res = mb.call_noexception("code", "load_file", "a",
-	 * "refac_util");
-	 * ErlLogger.debug("Wrangler's path is added to Erlang with result:" +
-	 * res.isOk() + "\t raw:" + res);
-	 * 
-	 * mb.call_noexception("application", "load", "a", "wrangler_app"); //
-	 * application:start(wrangler_app) res = mb.call_noexception("application",
-	 * "start", "a", "wrangler_app");
-	 * 
-	 * ErlLogger.debug("Wrangler app started:\n" + res);
-	 * 
-	 * } catch (IOException ioe) { ioe.printStackTrace(); throw new
-	 * CoreException(new Status(IStatus.ERROR, PLUGIN_ID,
-	 * "Could not load the ebin files!")); } catch (Exception e) {
-	 * e.printStackTrace(); } }
-	 */
+
+	private void initWrangler() throws CoreException { /*
+														 * try { Path pluginPath
+														 * = getPluginPath();
+														 * IPath
+														 * wranglerRootPath =
+														 * pluginPath
+														 * .append("wrangler");
+														 * String
+														 * wranglerEbinPath =
+														 * wranglerRootPath
+														 * .append("ebin")
+														 * .toOSString(); String
+														 * wranglerAppPath =
+														 * wranglerRootPath
+														 * .append("app")
+														 * .toOSString();
+														 * 
+														 * String
+														 * wranglerSrcPath =
+														 * wranglerRootPath
+														 * .append("erl")
+														 * .toOSString();
+														 * 
+														 * ErlLogger.debug(
+														 * "Wrangler beam files found at: "
+														 * + wranglerEbinPath);
+														 */
+		ErlideBackend mb = ErlangCore.getBackendManager().getIdeBackend();
+
+		ErlLogger.debug("Managed backend found:" + mb.getJavaNodeName());
+
+		/*
+		 * ErlangCode.addPathA(mb , wranglerEbinPath); ErlangCode .addPathA(mb,
+		 * wranglerSrcPath); ErlangCode .addPathA(mb, wranglerAppPath);
+		 * ErlLogger.debug( "Wrangler path has been added." );
+		 * 
+		 * RpcResult res = mb.call_noexception ("code", "load_file", "a",
+		 * "wrangler"); res = mb.call_noexception( "code", "load_file", "a",
+		 * "refac_util"); ErlLogger.debug(
+		 * "Wrangler's path is added to Erlang with result:" + res.isOk() +
+		 * "\t raw:" + res);
+		 */
+		RpcResult res = mb.call_noexception("wrangler", "init_eclipse", "",
+				new Object[0]);
+		/*
+		 * application :start(wrangler_app) res = mb.call_noexception
+		 * ("application", "start", "a", "wrangler_app");
+		 */
+		ErlLogger.debug("Wrangler app started:\n" + res);
+		/*
+		 * } catch (IOException ioe) { ioe.printStackTrace (); throw new
+		 * CoreException(new Status(IStatus.ERROR, PLUGIN_ID,
+		 * "Could not load the ebin files!" )); } catch (Exception e) {
+		 * e.printStackTrace(); } }
+		 */
+	}
 
 	/**
 	 * Returns with the Wrangler integration plugin path
@@ -103,7 +129,7 @@ public class Activator extends AbstractUIPlugin {
 	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		// initWrangler();
+		initWrangler();
 	}
 
 	/*
