deae2cf9799fec96b717ec4d44b678c3e6912150
Vlad Dumitrescu <vladdu55@gmail.com>
2010-04-19 10:26:08 +0000

    undo patch for #491

--- org.erlide.ui/src/org/erlide/ui/editors/erl/IErlangEditorActionDefinitionIds.java
+++ org.erlide.ui/src/org/erlide/ui/editors/erl/IErlangEditorActionDefinitionIds.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2006 Vlad Dumitrescu and others.
- * All rights reserved. This program and the accompanying materials 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at 
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Vlad Dumitrescu
  *     Alain O'Dea
@@ -16,7 +16,7 @@ package org.erlide.ui.editors.erl;
  * The {@link IErlangEditorActionDefinitionIds} class provides a set of
  * constants linking back to the actions defined in <code>plugin.xml</code>.
  * </p>
- *
+ * 
  * @author Vlad Dumitrescu
  */
 public class IErlangEditorActionDefinitionIds {
@@ -33,6 +33,5 @@ public class IErlangEditorActionDefinitionIds {
 	public static final String COMPILE = "org.erlide.ui.actions.compile";
 	public static final String CALLHIERARCHY = "org.erlide.ui.actions.callhierarchy";
 	public static final String SEND_TO_CONSOLE = "org.erlide.ui.actions.sendToConsole";
-	public static final String CLEAN_UP = "org.erlide.ui.actions.cleanUp";
 
 }
