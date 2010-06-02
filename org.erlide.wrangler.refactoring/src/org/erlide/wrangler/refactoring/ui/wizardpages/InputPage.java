5403c0d3e80ab058ae0949435dc0f46f93833a91
György Orosz <G.Orosz@kent.ac.uk>
2010-06-02 14:32:26 +0200

    Copyright notice is added.

--- org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/ui/wizardpages/InputPage.java
+++ org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/ui/wizardpages/InputPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 György Orosz.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     György Orosz - initial API and implementation
+ ******************************************************************************/
 package org.erlide.wrangler.refactoring.ui.wizardpages;
 
 import org.eclipse.jface.wizard.IWizardPage;
