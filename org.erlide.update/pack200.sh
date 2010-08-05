575b2790b38c9a3f5ea78e17d0975beafbeb15f8
Vlad Dumitrescu <vladdu55@gmail.com>
2010-08-05 14:33:56 +0200

    removed 'update' project

--- org.erlide.update/pack200.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-java -jar c:/progra~1/eclipse33/plugins/org.eclipse.equinox.launcher_1.0.1.R33x_v20080118.jar \
-  -application org.eclipse.update.core.siteOptimizer -digestBuilder \
-  -digestOutputDir=digest -siteXML=site.xml  -jarProcessor -processAll -repack -pack \
-  -outputDir . .
--- org.erlide.update/publish.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-scp -r features/ ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
-scp -r plugins/ ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
-scp web/site* ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update/web
-scp index.html ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
-scp site.xml ${1},erlide@frs.sourceforge.net:/home/frs/project/e/er/erlide/update
