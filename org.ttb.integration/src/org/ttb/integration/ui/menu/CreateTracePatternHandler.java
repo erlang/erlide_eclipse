ff99a4308ba8781a519bf4a639cb569789cbde24
Piotr Dorobisz <piotr.dorobisz@erlang-solutions.com>
2010-08-17 16:05:17 +0100

    add action to popup menu

---
+++ org.ttb.integration/src/org/ttb/integration/ui/menu/CreateTracePatternHandler.java
@@ -0,0 +1,34 @@
+package org.ttb.integration.ui.menu;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.ui.PlatformUI;
+import org.erlide.core.erlang.internal.ErlFunction;
+import org.ttb.integration.TtbBackend;
+import org.ttb.integration.mvc.model.TracePattern;
+
+public class CreateTracePatternHandler extends AbstractHandler {
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
+
+        if (selection instanceof ITreeSelection) {
+
+            Object firstElement = ((ITreeSelection) selection).getFirstElement();
+
+            if (firstElement instanceof ErlFunction) {
+                ErlFunction function = (ErlFunction) firstElement;
+                TracePattern tracePattern = new TracePattern();
+                tracePattern.setFunctionName(function.getFunctionName());
+                tracePattern.setModuleName(function.getModule().getModuleName());
+                tracePattern.setArity(function.getArity());
+                tracePattern.setEnabled(true);
+                TtbBackend.getInstance().addTracePattern(tracePattern);
+            }
+        }
+        return null;
+    }
+}
