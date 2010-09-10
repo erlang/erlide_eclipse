fee3f6450d5c20cfc0b9c170057fbd7cd9a43dc9
Vlad Dumitrescu <vladdu55@gmail.com>
2010-09-10 16:19:25 +0200

    simplified proj properties

--- org.erlide.core/src/org/erlide/core/erlang/IErlProject.java
+++ org.erlide.core/src/org/erlide/core/erlang/IErlProject.java
@@ -14,6 +14,7 @@ package org.erlide.core.erlang;
 import java.util.Collection;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.erlide.core.preferences.OldErlangProjectProperties;
@@ -43,88 +44,89 @@ import org.erlide.core.preferences.OldErlangProjectProperties;
  * one of these handles can be created via
  * <code>ErlangCore.create(project)</code>.
  * </p>
- * 
+ *
  * @see ErlangCore#createRoot(org.eclipse.core.resources.IProject)
  */
 public interface IErlProject extends IErlFolder {
 
-	/**
-	 * Returns the default output location for this project as a workspace-
-	 * relative absolute path.
-	 * <p>
-	 * The default output location is where class files are ordinarily generated
-	 * (and resource files, copied). Each source classpath entry can also
-	 * specify an output location for the generated class files (and copied
-	 * resource files) corresponding to compilation units under that source
-	 * folder. This makes it possible to arrange generated class files for
-	 * different source folders in different output folders, and not necessarily
-	 * the default output folder. This means that the generated class files for
-	 * the project may end up scattered across several folders, rather than all
-	 * in the default output folder (which is more standard).
-	 * </p>
-	 * 
-	 * @return the workspace-relative absolute path of the default output folder
-	 * @throws ErlModelException
-	 *             if this element does not exist
-	 * @see #setOutputLocation(org.eclipse.core.runtime.IPath, IProgressMonitor)
-	 */
-	IPath getOutputLocation() throws ErlModelException;
+    /**
+     * Returns the default output location for this project as a workspace-
+     * relative absolute path.
+     * <p>
+     * The default output location is where class files are ordinarily generated
+     * (and resource files, copied). Each source classpath entry can also
+     * specify an output location for the generated class files (and copied
+     * resource files) corresponding to compilation units under that source
+     * folder. This makes it possible to arrange generated class files for
+     * different source folders in different output folders, and not necessarily
+     * the default output folder. This means that the generated class files for
+     * the project may end up scattered across several folders, rather than all
+     * in the default output folder (which is more standard).
+     * </p>
+     *
+     * @return the workspace-relative absolute path of the default output folder
+     * @throws ErlModelException
+     *             if this element does not exist
+     * @see #setOutputLocation(org.eclipse.core.runtime.IPath, IProgressMonitor)
+     */
+    IPath getOutputLocation() throws ErlModelException;
 
-	/**
-	 * Returns the <code>IProject</code> on which this <code>IErlProject</code>
-	 * was created. This is handle-only method.
-	 * 
-	 * @return the <code>IProject</code> on which this <code>IErlProject</code>
-	 *         was created
-	 */
-	IProject getProject();
+    /**
+     * Returns the <code>IProject</code> on which this <code>IErlProject</code>
+     * was created. This is handle-only method.
+     *
+     * @return the <code>IProject</code> on which this <code>IErlProject</code>
+     *         was created
+     */
+    IProject getProject();
 
-	/**
-	 * Returns the names of the projects that are directly required by this
-	 * project. A project is required if it is in its classpath.
-	 * <p>
-	 * The project names are returned in the order they appear on the classpath.
-	 * 
-	 * @return the names of the projects that are directly required by this
-	 *         project in classpath order
-	 * @throws ErlModelException
-	 *             if this element does not exist or if an exception occurs
-	 *             while accessing its corresponding resource
-	 */
-	Collection<String> getRequiredProjectNames() throws ErlModelException;
+    /**
+     * Returns the names of the projects that are directly required by this
+     * project. A project is required if it is in its classpath.
+     * <p>
+     * The project names are returned in the order they appear on the classpath.
+     *
+     * @return the names of the projects that are directly required by this
+     *         project in classpath order
+     * @throws ErlModelException
+     *             if this element does not exist or if an exception occurs
+     *             while accessing its corresponding resource
+     * @throws CoreException
+     */
+    Collection<String> getRequiredProjectNames() throws CoreException;
 
-	/**
-	 * Sets the default output location of this project to the location
-	 * described by the given workspace-relative absolute path.
-	 * 
-	 * @param path
-	 *            the workspace-relative absolute path of the default output
-	 *            folder
-	 * @param monitor
-	 *            the progress monitor
-	 * 
-	 * @throws ErlModelException
-	 *             if the classpath could not be set. Reasons include:
-	 *             <ul>
-	 *             <li>This Erlang element does not exist
-	 *             (ELEMENT_DOES_NOT_EXIST)</li>
-	 *             <li>The path refers to a location not contained in this
-	 *             project ( <code>PATH_OUTSIDE_PROJECT</code>)
-	 *             <li>The path is not an absolute path (
-	 *             <code>RELATIVE_PATH</code>)
-	 *             <li>The path is nested inside a package fragment root of this
-	 *             project ( <code>INVALID_PATH</code>)
-	 *             <li>The output location is being modified during resource
-	 *             change event notification (CORE_EXCEPTION)
-	 *             </ul>
-	 * @see #getOutputLocation()
-	 * @see IClasspathEntry#getOutputLocation()
-	 */
-	void setOutputLocation(IPath path, IProgressMonitor monitor)
-			throws ErlModelException;
+    /**
+     * Sets the default output location of this project to the location
+     * described by the given workspace-relative absolute path.
+     *
+     * @param path
+     *            the workspace-relative absolute path of the default output
+     *            folder
+     * @param monitor
+     *            the progress monitor
+     *
+     * @throws ErlModelException
+     *             if the classpath could not be set. Reasons include:
+     *             <ul>
+     *             <li>This Erlang element does not exist
+     *             (ELEMENT_DOES_NOT_EXIST)</li>
+     *             <li>The path refers to a location not contained in this
+     *             project ( <code>PATH_OUTSIDE_PROJECT</code>)
+     *             <li>The path is not an absolute path (
+     *             <code>RELATIVE_PATH</code>)
+     *             <li>The path is nested inside a package fragment root of this
+     *             project ( <code>INVALID_PATH</code>)
+     *             <li>The output location is being modified during resource
+     *             change event notification (CORE_EXCEPTION)
+     *             </ul>
+     * @see #getOutputLocation()
+     * @see IClasspathEntry#getOutputLocation()
+     */
+    void setOutputLocation(IPath path, IProgressMonitor monitor)
+            throws ErlModelException;
 
-	Collection<IErlModule> getModulesAndHeaders() throws ErlModelException;
+    Collection<IErlModule> getModulesAndHeaders() throws ErlModelException;
 
-	OldErlangProjectProperties getProperties();
+    OldErlangProjectProperties getProperties();
 
 }
