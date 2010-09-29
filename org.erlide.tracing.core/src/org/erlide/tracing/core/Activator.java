804df762e4d6e16137573835a6d66cbe97005bb6
Vlad Dumitrescu <vladdu55@gmail.com>
2010-09-29 20:35:45 +0200

    finished renaming ttb plugins

--- org.erlide.tracing.core/src/org/erlide/tracing/core/Activator.java
+++ org.erlide.tracing.core/src/org/erlide/tracing/core/Activator.java
@@ -15,7 +15,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "org.ttb.integration"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "org.erlide.tracing.core"; //$NON-NLS-1$
 
     // The shared instance
     private static Activator plugin;
