ad9ebf9fcd88ff4cca5bb4b5c4b0023b9c51dcf4
Vlad Dumitrescu <vladdu55@gmail.com>
2010-11-15 13:28:20 +0100

    moved tests to right package

--- org.erlide.jinterface.tests/src/org/erlide/jinterface/backend/RuntimeVersionTest.java
+++ org.erlide.jinterface.tests/src/org/erlide/jinterface/backend/RuntimeVersionTest.java
@@ -1,4 +1,4 @@
-package org.erlide.runtime.backend;
+package org.erlide.jinterface.backend;
 
 import junit.framework.Assert;
 
--- org.erlide.jinterface.tests/src/org/erlide/jinterface/backend/UtilTest.java
+++ org.erlide.jinterface.tests/src/org/erlide/jinterface/backend/UtilTest.java
@@ -1,4 +1,4 @@
-package org.erlide.runtime.backend;
+package org.erlide.jinterface.backend;
 
 import static org.junit.Assert.assertEquals;
 
