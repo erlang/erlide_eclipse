0691b4d611061a3d79c44ec22aec803adc745ea7
Vlad Dumitrescu <vladdu55@gmail.com>
2010-12-10 14:33:53 +0100

    removed commented-out code

--- org.erlide.core/src/org/erlide/core/ExtensionHelper.java
+++ org.erlide.core/src/org/erlide/core/ExtensionHelper.java
@@ -27,38 +27,6 @@ public class ExtensionHelper {
 
     // TODO list the extension points
     public static final String EDITOR_LISTENER = "org.erlide.editor_listener";
-    // public final static String PYDEV_COMPLETION =
-    // "org.python.pydev.pydev_completion";
-    // public final static String PYDEV_BUILDER =
-    // "org.python.pydev.pydev_builder";
-    // public final static String PYDEV_MODULES_OBSERVER =
-    // "org.python.pydev.pydev_modules_observer";
-    // public final static String PYDEV_INTERPRETER_OBSERVER =
-    // "org.python.pydev.pydev_interpreter_observer";
-    // public final static String PYDEV_MANAGER_OBSERVER =
-    // "org.python.pydev.pydev_manager_observer";
-    // public final static String PYDEV_PARSER_OBSERVER =
-    // "org.python.pydev.parser.pydev_parser_observer";
-    // public static final String PYDEV_CTRL_1 =
-    // "org.python.pydev.pydev_ctrl_1";
-    // public static final String PYDEV_SIMPLE_ASSIST =
-    // "org.python.pydev.pydev_simpleassist";
-    // public static final String PYDEV_ORGANIZE_IMPORTS =
-    // "org.python.pydev.pydev_organize_imports";
-    // public static final String PYDEV_REFACTORING =
-    // "org.python.pydev.pydev_refactoring";
-    // public static final String PYDEV_QUICK_OUTLINE =
-    // "org.python.pydev.pydev_quick_outline";
-    // public static final String PYDEV_FORMATTER =
-    // "org.python.pydev.pydev_formatter";
-    // public static final String PYDEV_GLOBALS_BROWSER =
-    // "org.python.pydev.pydev_globals_browser";
-    // public static final String PYDEV_DEBUG_PREFERENCES_PAGE =
-    // "org.python.pydev.pydev_debug_preferences_page";
-    // public static final String PYDEV_HOVER = "org.python.pydev.pydev_hover";
-
-    // debug
-    public static final String PYDEV_DEBUG_CONSOLE_INPUT_LISTENER = "org.python.pydev.debug.pydev_debug_console_input_listener";
 
     public static IExtension[] getExtensions(final String type) {
         IExtension[] extensions = extensionsCache.get(type);
