53f54c96a71b685a15ae28a9e6a67370be354eb5
Vlad Dumitrescu <vladdu55@gmail.com>
2010-12-17 11:18:57 +0100

    cleanup unused code

--- org.erlide.jinterface/src/org/erlide/jinterface/backend/NoBackendException.java
+++ org.erlide.jinterface/src/org/erlide/jinterface/backend/NoBackendException.java
@@ -13,10 +13,6 @@ public class NoBackendException extends BackendException {
 
     private static final long serialVersionUID = 1L;
 
-    public NoBackendException(final Exception e) {
-        super(e);
-    }
-
     public NoBackendException() {
         super("The Erlang backend is not running. Functionality is limited.");
     }
