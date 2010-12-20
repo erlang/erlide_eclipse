f08327e05e83954805a8fd256637fbfd28aa4394
Jakob C <jakobce@gmail.com>
2010-12-20 12:09:26 +0100

    wip

---
+++ org.erlide.ui/src/RunDialyzerCommandHandler.java
@@ -0,0 +1,14 @@
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+public class RunDialyzerCommandHandler extends AbstractHandler implements
+        IHandler {
+
+    public Object execute(final ExecutionEvent event) throws ExecutionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
