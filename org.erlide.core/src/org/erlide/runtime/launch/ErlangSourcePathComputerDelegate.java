6e6dfba140550a19312499b2dba32d48b1b96deb
Vlad Dumitrescu <vladdu55@gmail.com>
2011-02-02 14:24:09 +0100

    moved methods from BackendUtils

--- org.erlide.core/src/org/erlide/runtime/launch/ErlangSourcePathComputerDelegate.java
+++ org.erlide.core/src/org/erlide/runtime/launch/ErlangSourcePathComputerDelegate.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -13,7 +15,6 @@ import org.eclipse.debug.core.sourcelookup.ISourcePathComputerDelegate;
 import org.eclipse.debug.core.sourcelookup.containers.ProjectSourceContainer;
 import org.eclipse.debug.core.sourcelookup.containers.WorkspaceSourceContainer;
 import org.erlide.core.erlang.ErlangCore;
-import org.erlide.core.erlang.util.BackendUtils;
 import org.erlide.jinterface.backend.RuntimeInfo;
 
 public class ErlangSourcePathComputerDelegate implements
@@ -25,7 +26,7 @@ public class ErlangSourcePathComputerDelegate implements
         final List<ISourceContainer> containers = new ArrayList<ISourceContainer>();
         final String projectNames = configuration.getAttribute(
                 ErlLaunchAttributes.PROJECTS, "");
-        final IProject[] projects = BackendUtils.getProjects(projectNames);
+        final IProject[] projects = getProjects(projectNames);
         for (final IProject p : projects) {
             containers.add(new ProjectSourceContainer(p, false));
         }
@@ -39,6 +40,25 @@ public class ErlangSourcePathComputerDelegate implements
         containers
                 .add(new ErlangOtpSourceContainer(new Path(info.getOtpHome())));
         return containers.toArray(new ISourceContainer[containers.size()]);
+    }
+
+    private static IProject[] getProjects(final String attribute) {
+        final String[] projectNames = attribute.split(";");
+        return getProjects(projectNames);
+    }
+
+    private static IProject[] getProjects(final String[] projectNames) {
+        final List<IProject> projects = new ArrayList<IProject>();
+        final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+        for (final String s : projectNames) {
+            if (s != null && s.length() > 0) {
+                final IProject p = root.getProject(s);
+                if (p != null) {
+                    projects.add(p);
+                }
+            }
+        }
+        return projects.toArray(new IProject[projects.size()]);
     }
 
 }
