ad1909e7d540c38568a6308938fbc1e9549a45f2
Jakob C <jakobce@gmail.com>
2011-03-14 17:24:11 +0100

    Forgot wrangler...

--- org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/selection/internal/AbstractErlSelection.java
+++ org.erlide.wrangler.refactoring/src/org/erlide/wrangler/refactoring/selection/internal/AbstractErlSelection.java
@@ -31,35 +31,36 @@ import com.ericsson.otp.erlang.OtpErlangString;
  */
 public abstract class AbstractErlSelection implements IErlSelection {
 
-    protected IFile file;
+	protected IFile file;
 
-    public boolean isEmpty() {
-        return false;
-    }
+	public boolean isEmpty() {
+		return false;
+	}
 
-    public OtpErlangList getSearchPath() {
-        final IProject project = file.getProject();
-        final IErlModel model = ErlangCore.getModel();
-        final IErlProject actualProject = model.getErlangProject(project);
-        final IPath projectLocation = actualProject.getProject().getLocation();
+	public OtpErlangList getSearchPath() {
+		final IProject project = file.getProject();
+		final IErlModel model = ErlangCore.getModel();
+		final IErlProject actualProject = model.getErlangProject(project);
+		final IPath projectLocation = actualProject.getWorkspaceProject()
+				.getLocation();
 
-        final Collection<IPath> sourcDirs = actualProject.getSourceDirs();
-        final OtpErlangString[] searchPath = new OtpErlangString[sourcDirs
-                .size()];
-        int i = 0;
-        for (final IPath src : sourcDirs) {
-            searchPath[i++] = new OtpErlangString(projectLocation.append(src)
-                    .toOSString());
-        }
-        return new OtpErlangList(searchPath);
-    }
+		final Collection<IPath> sourcDirs = actualProject.getSourceDirs();
+		final OtpErlangString[] searchPath = new OtpErlangString[sourcDirs
+				.size()];
+		int i = 0;
+		for (final IPath src : sourcDirs) {
+			searchPath[i++] = new OtpErlangString(projectLocation.append(src)
+					.toOSString());
+		}
+		return new OtpErlangList(searchPath);
+	}
 
-    public String getFilePath() {
-        return file.getLocation().toOSString();
-    }
+	public String getFilePath() {
+		return file.getLocation().toOSString();
+	}
 
-    public IFile getFile() {
-        return file;
-    }
+	public IFile getFile() {
+		return file;
+	}
 
 }
