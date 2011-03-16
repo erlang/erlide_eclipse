db687fd300b9c6057ee6b198c741b493835d9f40
Aleksandra Lipiec <aleksandra.lipiec@erlang-solutions.com>
2011-03-16 14:58:57 +0100

    improved HTML viewer navigation

--- org.erlide.cover.core/src/org/erlide/cover/views/model/ICoverageObject.java
+++ org.erlide.cover.core/src/org/erlide/cover/views/model/ICoverageObject.java
@@ -10,48 +10,140 @@ import java.io.Serializable;
  */
 public interface ICoverageObject extends Serializable {
 
+    /**
+     * Search children by name
+     * @param name
+     * @return
+     */
     public ICoverageObject findChild(String name);
 
+    /**
+     * Return a path to the html report for the element
+     * @return
+     */
     public String getHtmlPath();
 
+    /**
+     * Set a path to the html report for the element
+     * @param htmlPath
+     */
     public void setHtmlPath(final String htmlPath);
 
+    /**
+     * Find next sibling to the given child
+     * @param name
+     * @return
+     */
     public ICoverageObject getPrevSiblingTo(String name);
 
+    /**
+     * Find previous sibling to the given child
+     * @param name
+     * @return
+     */
     public ICoverageObject getNextSiblingTo(String name);
 
+    /**
+     * Returns element's parent
+     * @return
+     */
     public ICoverageObject getParent();
 
+    /**
+     * Set the elements parent
+     * @param parent
+     */
     public void setParent(ICoverageObject parent);
 
+    /**
+     * add a child for the element
+     * @param name
+     * @param child
+     */
     public void addChild(String name, ICoverageObject child);
 
+    /**
+     * removes a child by name
+     * @param name
+     */
     public void removeChild(String name);
 
+    /**
+     * remove all children of the element
+     */
     public void removeAllChildren();
 
+    /**
+     * return children table
+     * @return
+     */
     public ICoverageObject[] getChildren();
 
+    /**
+     * check if the element has any children
+     * @return
+     */
     public boolean hasChildren();
 
+    /**
+     * check the element's type
+     * @return
+     */
     public ObjectType getType();
 
+    /**
+     * return the element's label
+     * @return
+     */
     public String getLabel();
 
+    /**
+     * set the element's label
+     * @param label
+     */
     public void setLabel(String label);
 
+    /**
+     * return the number of the lines
+     * @return
+     */
     public int getLinesCount();
 
+    /**
+     * set the number of the lines (total)
+     * @param count
+     */
     public void setLiniesCount(int count);
 
+    /**
+     * return the number of covered lines
+     * @return
+     */
     public int getCoverCount();
 
+    /**
+     * set the number of covered lines
+     * @param count
+     */
     public void setCoverCount(int count);
 
+    /**
+     * get coverage ratio
+     * @return
+     */
     public double getPercentage();
 
+    /**
+     * get array of children names
+     * @return
+     */
     public String[] getStringArray();
 
+    /**
+     * serch the element's tree to find an element withc specified name
+     * @param name
+     * @return
+     */
     public ICoverageObject treeSearch(String name);
     
 }
--- org.erlide.cover.ui/src/org/erlide/cover/ui/views/util/BrowserDialog.java
+++ org.erlide.cover.ui/src/org/erlide/cover/ui/views/util/BrowserDialog.java
@@ -1,7 +1,10 @@
 package org.erlide.cover.ui.views.util;
 
+import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.LocationEvent;
+import org.eclipse.swt.browser.LocationListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
@@ -37,6 +40,7 @@ public class BrowserDialog extends Dialog {
     private ICoverageObject object;
 
     private String url;
+    private Logger log; // logger
 
     public BrowserDialog(final Shell parent) {
         this(parent, SWT.NULL);
@@ -44,6 +48,7 @@ public class BrowserDialog extends Dialog {
 
     public BrowserDialog(final Shell parent, final int style) {
         super(parent, style);
+        log = Logger.getLogger(getClass());
     }
 
     public void open() {
@@ -62,6 +67,7 @@ public class BrowserDialog extends Dialog {
 
     /**
      * Sets object connected with current path
+     * 
      * @param path
      */
     public void setObject(final ICoverageObject obj) {
@@ -96,6 +102,7 @@ public class BrowserDialog extends Dialog {
         browserLData.heightHint = 500;
         browser.setLayoutData(browserLData);
         browser.setUrl(url);
+        browser.addLocationListener(locationListener);
 
         comp = new Composite(dialogShell, SWT.NONE);
         GridLayout compLayout = new GridLayout();
@@ -123,6 +130,16 @@ public class BrowserDialog extends Dialog {
         next.addSelectionListener(nextListener);
         next.setImage(Activator.getImageDescriptor(Images.NEXT).createImage());
 
+        if (object.getParent() != null) {
+            if (object.getParent().getNextSiblingTo(object.getLabel()) == null)
+                next.setEnabled(false);
+            if (object.getParent().getPrevSiblingTo(object.getLabel()) == null)
+                prev.setEnabled(false);
+        } else {
+            next.setEnabled(false);
+            prev.setEnabled(false);
+        }
+
         home = new Button(comp, SWT.PUSH | SWT.CENTER | SWT.FILL);
         GridData homeLData = new GridData();
         home.setLayoutData(homeLData);
@@ -140,20 +157,24 @@ public class BrowserDialog extends Dialog {
         dialogShell.setLocation(getParent().toDisplay(100, 100));
 
     }
-    
+
     private SelectionListener prevListener = new SelectionListener() {
 
         public void widgetSelected(SelectionEvent e) {
             if (object.getParent() != null) {
                 ICoverageObject sib = object.getParent().getPrevSiblingTo(
                         object.getLabel());
-                if(sib == null) {
-                    CoverageHelper.reportInfo("No previous pages");
+                if (sib == null) {
+                    // CoverageHelper.reportInfo("No previous pages");
                     return;
                 }
                 setObject(sib);
                 browser.setUrl(url);
                 titleLabel.setText(url);
+                // disable buttons
+                next.setEnabled(true);
+                if (object.getParent().getPrevSiblingTo(object.getLabel()) == null)
+                    prev.setEnabled(false);
             }
         }
 
@@ -169,13 +190,17 @@ public class BrowserDialog extends Dialog {
             if (object.getParent() != null) {
                 ICoverageObject sib = object.getParent().getNextSiblingTo(
                         object.getLabel());
-                if(sib == null){
-                    CoverageHelper.reportInfo("No further pages");
+                if (sib == null) {
+                    // CoverageHelper.reportInfo("No further pages");
                     return;
                 }
                 setObject(sib);
                 browser.setUrl(url);
                 titleLabel.setText(url);
+                // disable buttons
+                prev.setEnabled(true);
+                if (object.getParent().getNextSiblingTo(object.getLabel()) == null)
+                    next.setEnabled(false);
             }
         }
 
@@ -192,6 +217,12 @@ public class BrowserDialog extends Dialog {
                 setObject(object.getParent());
                 browser.setUrl(url);
                 titleLabel.setText(url);
+
+                if (object.getParent().getNextSiblingTo(object.getLabel()) == null)
+                    next.setEnabled(false);
+                if (object.getParent().getPrevSiblingTo(object.getLabel()) == null)
+                    prev.setEnabled(false);
+
             }
         }
 
@@ -207,10 +238,49 @@ public class BrowserDialog extends Dialog {
             setObject(StatsTreeModel.getInstance().getRoot());
             browser.setUrl(url);
             titleLabel.setText(url);
+            next.setEnabled(false);
+            prev.setEnabled(false);
         }
 
         public void widgetDefaultSelected(SelectionEvent e) {
             // TODO Auto-generated method stub
+        }
+
+    };
+
+    private LocationListener locationListener = new LocationListener() {
+
+        public void changing(LocationEvent event) {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void changed(LocationEvent event) {
+            if (object == null)
+                return;
+            log.debug(event.getSource());
+            String newUrl = browser.getUrl();
+            titleLabel.setText(newUrl);
+            String name = newUrl.substring(newUrl.lastIndexOf('/') + 1,
+                    newUrl.length() - 5);
+            log.debug(name);
+            if (name.startsWith("mod_"))
+                name = name.substring(4);
+
+            ICoverageObject newObj;
+            if ((newObj = object.treeSearch(name)) != null) { // TODO ->
+                                                              // findChild
+                setObject(newObj);
+                if (object.getParent().getNextSiblingTo(object.getLabel()) == null)
+                    next.setEnabled(false);
+                else
+                    next.setEnabled(true);
+                if (object.getParent().getPrevSiblingTo(object.getLabel()) == null)
+                    prev.setEnabled(false);
+                else
+                    prev.setEnabled(true);
+            }
+
         }
 
     };
