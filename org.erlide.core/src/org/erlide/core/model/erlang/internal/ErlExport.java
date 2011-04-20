359dd6cd12ede9996371ad532b1fe2b564b58640
Jakob C <jakobce@gmail.com>
2011-04-20 15:22:02 +0200

    http://www.assembla.com/spaces/erlide/tickets/824-outline---sometimes-no-text

--- org.erlide.core/src/org/erlide/core/model/erlang/internal/ErlExport.java
+++ org.erlide.core/src/org/erlide/core/model/erlang/internal/ErlExport.java
@@ -7,6 +7,7 @@ import com.ericsson.otp.erlang.OtpErlangList;
 
 public class ErlExport extends ErlImportExport implements IErlExport {
 
+    private static final int LABEL_LENGTH_LIMIT = 100;
     private final String functions;
 
     public ErlExport(final IParent parent, final OtpErlangList functionList,
@@ -22,6 +23,19 @@ public class ErlExport extends ErlImportExport implements IErlExport {
     @Override
     public String toString() {
         return getName() + ": " + functions;
+    }
+
+    @Override
+    public String getLabelString() {
+        String s = functions;
+        if (s.length() > LABEL_LENGTH_LIMIT) {
+            int i = s.indexOf(',', LABEL_LENGTH_LIMIT);
+            if (i == -1) {
+                i = LABEL_LENGTH_LIMIT;
+            }
+            s = s.substring(0, i) + "...";
+        }
+        return s;
     }
 
 }
