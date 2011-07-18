a1e5087ea45a8d856716153d6feaee8097970397
Vlad Dumitrescu <vladdu55@gmail.com>
2011-07-18 13:51:49 +0200

    432-cannot-paste-into-console-window-unless-you-first-type-another-char

--- org.erlide.ui/src/org/erlide/ui/console/ErlangConsolePage.java
+++ org.erlide.ui/src/org/erlide/ui/console/ErlangConsolePage.java
@@ -47,6 +47,8 @@ import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
@@ -168,7 +170,7 @@ public class ErlangConsolePage extends Page implements IAdaptable,
     }
 
     void createInputField(final KeyEvent first, final boolean isPaste) {
-        if (first.character == SWT.ESC) {
+        if (!isPaste && first.character == SWT.ESC) {
             return;
         }
         try {
@@ -200,7 +202,7 @@ public class ErlangConsolePage extends Page implements IAdaptable,
 
                 @Override
                 public void keyPressed(final KeyEvent e) {
-                    final boolean historyMode = (e.stateMask & SWT.CTRL) == SWT.CTRL;
+                    final boolean ctrlPressed = (e.stateMask & SWT.CTRL) == SWT.CTRL;
                     if (e.keyCode == 13
                             && isInputComplete()
                             && consoleInput.getSelection().x == consoleInput
@@ -223,14 +225,14 @@ public class ErlangConsolePage extends Page implements IAdaptable,
                             consoleInput.setTopIndex(lineCount - visibleLines
                                     + 1);
                         }
-                    } else if (historyMode && e.keyCode == SWT.ARROW_UP) {
+                    } else if (ctrlPressed && e.keyCode == SWT.ARROW_UP) {
                         history.prev();
                         final String s = history.get();
                         consoleInput.setText(s);
                         consoleInput.setSelection(consoleInput.getText()
                                 .length());
                         fixPosition(container);
-                    } else if (historyMode && e.keyCode == SWT.ARROW_DOWN) {
+                    } else if (ctrlPressed && e.keyCode == SWT.ARROW_DOWN) {
                         history.next();
                         final String s = history.get();
                         consoleInput.setText(s);
@@ -384,13 +386,17 @@ public class ErlangConsolePage extends Page implements IAdaptable,
     @Override
     public void createControl(final Composite parent) {
         consoleOutputViewer = new SourceViewer(parent, null, SWT.V_SCROLL
-                | SWT.H_SCROLL | SWT.MULTI | SWT.READ_ONLY);
+                | SWT.H_SCROLL | SWT.MULTI);
         consoleText = (StyledText) consoleOutputViewer.getControl();
         consoleText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true,
                 2, 1));
 
         consoleText.setFont(JFaceResources.getTextFont());
-        consoleText.setEditable(false);
+        consoleText.addVerifyListener(new VerifyListener() {
+            public void verifyText(final VerifyEvent e) {
+                e.doit = false;
+            }
+        });
         consoleText.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(final KeyEvent e) {
@@ -540,7 +546,12 @@ public class ErlangConsolePage extends Page implements IAdaptable,
         setGlobalAction(actionBars, ActionFactory.COPY.getId(), action);
 
         action = new TextViewerAction(consoleOutputViewer,
-                ITextOperationTarget.PASTE);
+                ITextOperationTarget.PASTE) {
+            @Override
+            public void run() {
+                createInputField(null, true);
+            }
+        };
         action.configureAction(ConsoleMessages.TextConsolePage_PasteText,
                 ConsoleMessages.TextConsolePage_PasteDescrip,
                 ConsoleMessages.TextConsolePage_PasteDescrip);
