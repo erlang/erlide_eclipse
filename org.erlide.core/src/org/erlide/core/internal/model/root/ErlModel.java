bdd796797039c4dcf8c6bd7a48b00917b7277f28
Vlad Dumitrescu <vladdu55@gmail.com>
2011-09-05 13:14:24 +0200

    extracted ErlModelDeltaManager from ErlModel

--- org.erlide.core/src/org/erlide/core/internal/model/root/ErlModel.java
+++ org.erlide.core/src/org/erlide/core/internal/model/root/ErlModel.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -61,7 +60,6 @@ import org.erlide.core.model.root.IErlParser;
 import org.erlide.core.model.root.IErlProject;
 import org.erlide.core.model.root.IOpenable;
 import org.erlide.core.model.root.IParent;
-import org.erlide.core.model.root.IWorkingCopy;
 import org.erlide.core.model.util.ElementChangedEvent;
 import org.erlide.core.model.util.ErlangFunction;
 import org.erlide.core.model.util.ErlideUtil;
@@ -95,29 +93,11 @@ public class ErlModel extends Openable implements IErlModel {
     private final IPathVariableChangeListener fPathVariableChangeListener;
 
     /**
-     * Turns delta firing on/off. By default it is on.
-     */
-    protected boolean fFire = true;
-
-    /**
-     * Queue of reconcile deltas on working copies that have yet to be fired.
-     * This is a table form IWorkingCopy to IErlElementDelta
-     */
-    private final HashMap<IWorkingCopy, IErlElementDelta> reconcileDeltas = new HashMap<IWorkingCopy, IErlElementDelta>();
-
-    /**
      * Listeners for element changes
      */
-    protected List<IElementChangedListener> elementChangedListeners = new ArrayList<IElementChangedListener>();
+    final List<IElementChangedListener> elementChangedListeners = new ArrayList<IElementChangedListener>();
 
-    /**
-     * Queue of deltas created explicitly by the model that have yet to be
-     * fired.
-     */
-    private final List<IErlElementDelta> erlModelDeltas = Collections
-            .synchronizedList(new ArrayList<IErlElementDelta>());
-
-    public static final int DEFAULT_CHANGE_EVENT = 0; // must not collide with
+    private final ErlModelDeltaManager deltaManager;
 
     public static boolean verbose = Boolean.getBoolean("erlide.model.verbose");
 
@@ -146,6 +126,7 @@ public class ErlModel extends Openable implements IErlModel {
         pvm.addChangeListener(fPathVariableChangeListener);
         final IResourceChangeListener listener = new ResourceChangeListener();
         workspace.addResourceChangeListener(listener);
+        deltaManager = new ErlModelDeltaManager(this);
     }
 
     @Override
@@ -624,182 +605,13 @@ public class ErlModel extends Openable implements IErlModel {
     }
 
     /**
-     * Fire Model deltas, flushing them after the fact. If the firing mode has
-     * been turned off, this has no effect.
-     */
-    private void fire(final IErlElementDelta customDeltas, final int eventType) {
-        if (fFire) {
-            IErlElementDelta deltaToNotify;
-            if (customDeltas == null) {
-                deltaToNotify = mergeDeltas(erlModelDeltas);
-            } else {
-                deltaToNotify = customDeltas;
-            }
-
-            final IElementChangedListener[] listeners;
-            final int listenerCount;
-            final int[] listenerMask;
-            // Notification
-            synchronized (elementChangedListeners) {
-                listeners = new IElementChangedListener[elementChangedListeners
-                        .size()];
-                elementChangedListeners.toArray(listeners);
-                listenerCount = listeners.length;
-                listenerMask = null;
-            }
-
-            switch (eventType) {
-            case DEFAULT_CHANGE_EVENT:
-                // firePreAutoBuildDelta(deltaToNotify, listeners, listenerMask,
-                // listenerCount);
-                firePostChangeDelta(deltaToNotify, listeners, listenerMask,
-                        listenerCount);
-                fireReconcileDelta(listeners, listenerMask, listenerCount);
-                break;
-            // case ElementChangedEvent.PRE_AUTO_BUILD :
-            // firePreAutoBuildDelta(deltaToNotify, listeners, listenerMask,
-            // listenerCount);
-            // break;
-            case ElementChangedEvent.POST_CHANGE:
-                firePostChangeDelta(deltaToNotify, listeners, listenerMask,
-                        listenerCount);
-                fireReconcileDelta(listeners, listenerMask, listenerCount);
-                break;
-            case ElementChangedEvent.POST_RECONCILE:
-                fireReconcileDelta(listeners, listenerMask, listenerCount);
-                break;
-            case ElementChangedEvent.POST_SHIFT:
-                fireShiftEvent(deltaToNotify, listeners, listenerMask,
-                        listenerCount);
-                return;
-            }
-        }
-    }
-
-    private void firePostChangeDelta(final IErlElementDelta deltaToNotify,
-            final IElementChangedListener[] listeners,
-            final int[] listenerMask, final int listenerCount) {
-
-        // post change deltas
-        if (verbose) {
-            System.out
-                    .println("FIRING POST_CHANGE Delta [" + Thread.currentThread() + "]:"); //$NON-NLS-1$//$NON-NLS-2$
-            System.out
-                    .println(deltaToNotify == null ? "<NONE>" : deltaToNotify.toString()); //$NON-NLS-1$
-        }
-        if (deltaToNotify != null) {
-            // flush now so as to keep listener reactions to post their own
-            // deltas for
-            // subsequent iteration
-            flush();
-            notifyListeners(deltaToNotify, ElementChangedEvent.POST_CHANGE,
-                    listeners, listenerMask, listenerCount);
-        }
-    }
-
-    private void fireReconcileDelta(final IElementChangedListener[] listeners,
-            final int[] listenerMask, final int listenerCount) {
-        final IErlElementDelta deltaToNotify = mergeDeltas(reconcileDeltas
-                .values());
-        if (verbose) {
-            System.out
-                    .println("FIRING POST_RECONCILE Delta [" + Thread.currentThread() + "]:"); //$NON-NLS-1$//$NON-NLS-2$
-            System.out
-                    .println(deltaToNotify == null ? "<NONE>" : deltaToNotify.toString()); //$NON-NLS-1$
-        }
-        if (deltaToNotify != null) {
-            // flush now so as to keep listener reactions to post their own
-            // deltas for
-            // subsequent iteration
-            reconcileDeltas.clear();
-            notifyListeners(deltaToNotify, ElementChangedEvent.POST_RECONCILE,
-                    listeners, listenerMask, listenerCount);
-        }
-    }
-
-    private void fireShiftEvent(final IErlElementDelta deltaToNotify,
-            final IElementChangedListener[] listeners,
-            final int[] listenerMask, final int listenerCount) {
-
-        // post change deltas
-        if (verbose) {
-            System.out
-                    .println("FIRING POST_SHIFT event [" + Thread.currentThread() + "]:"); //$NON-NLS-1$//$NON-NLS-2$
-            System.out
-                    .println(deltaToNotify == null ? "<NONE>" : deltaToNotify.toString()); //$NON-NLS-1$
-        }
-        if (deltaToNotify != null) {
-            flush();
-            notifyListeners(deltaToNotify, ElementChangedEvent.POST_SHIFT,
-                    listeners, listenerMask, listenerCount);
-        }
-    }
-
-    private IErlElementDelta mergeDeltas(
-            final Collection<IErlElementDelta> deltas) {
-
-        synchronized (deltas) {
-            if (deltas.size() == 0) {
-                return null;
-            }
-            if (deltas.size() == 1) {
-                return deltas.iterator().next();
-            }
-            if (deltas.size() <= 1) {
-                return null;
-            }
-
-            final Iterator<IErlElementDelta> iterator = deltas.iterator();
-            final IErlElement cRoot = getErlangModel();
-            final ErlElementDelta rootDelta = new ErlElementDelta(0, 0, cRoot);
-            boolean insertedTree = false;
-            while (iterator.hasNext()) {
-                final ErlElementDelta delta = (ErlElementDelta) iterator.next();
-                final IErlElement element = delta.getElement();
-                if (cRoot.equals(element)) {
-                    final IErlElementDelta[] children = delta
-                            .getChildren(IErlElementDelta.ALL);
-                    for (final IErlElementDelta element0 : children) {
-                        final ErlElementDelta projectDelta = (ErlElementDelta) element0;
-                        rootDelta.insertDeltaTree(projectDelta.getElement(),
-                                projectDelta);
-                        insertedTree = true;
-                    }
-                    final IResourceDelta[] resourceDeltas = delta
-                            .getResourceDeltas();
-                    if (resourceDeltas != null) {
-                        for (final IResourceDelta element0 : resourceDeltas) {
-                            rootDelta.addResourceDelta(element0);
-                            insertedTree = true;
-                        }
-                    }
-                } else {
-                    rootDelta.insertDeltaTree(element, delta);
-                    insertedTree = true;
-                }
-            }
-            if (insertedTree) {
-                return rootDelta;
-            }
-            return null;
-        }
-    }
-
-    /**
      * Registers the given delta with this manager. This API is to be used to
-     * registered deltas that are created explicitly by the C Model. Deltas
+     * registered deltas that are created explicitly by the Erlang Model. Deltas
      * created as translations of <code>IResourceDeltas</code> are to be
      * registered with <code>#registerResourceDelta</code>.
      */
     public void registerModelDelta(final IErlElementDelta delta) {
-        erlModelDeltas.add(delta);
-    }
-
-    /**
-     * Flushes all deltas without firing them.
-     */
-    protected void flush() {
-        erlModelDeltas.clear();
+        deltaManager.erlModelDeltas.add(delta);
     }
 
     public void notifyListeners(final IErlElementDelta deltaToNotify,
