231ccc01d102f28cdb1a1d26bafa98467453aa11
Aleksandra Lipiec <aleksandra.lipiec@erlang-solutions.com>
2011-10-11 16:30:41 +0200

    dependency to JDT removed

--- org.erlide.cover.ui/src/org/erlide/cover/ui/Images.java
+++ org.erlide.cover.ui/src/org/erlide/cover/ui/Images.java
@@ -15,5 +15,6 @@ public class Images {
     public static final String NEXT = PATH_PREFIX + "next.png";
     public static final String UP = PATH_PREFIX + "up.png";
     public static final String HOME = PATH_PREFIX + "home.png";
+    public static final String FUNCTION = PATH_PREFIX + "methpri_obj.gif";
 
 }
