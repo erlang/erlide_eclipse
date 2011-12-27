e365750db1a9a570c6a37486a8af4ee5f11e2dea
Vlad Dumitrescu <vladdu55@gmail.com>
2011-12-27 23:49:40 +0100

    simplify BackendCore

--- org.erlide.core/src/org/erlide/core/backend/BackendCore.java
+++ org.erlide.core/src/org/erlide/core/backend/BackendCore.java
@@ -7,7 +7,6 @@ import org.erlide.core.internal.backend.BackendManager;
 
 public class BackendCore {
 
-    private static IBackendFactory backendFactory;
     private static RuntimeInfoManager runtimeInfoManager;
     private static IBackendManager backendManager;
 
@@ -18,21 +17,14 @@ public class BackendCore {
         return runtimeInfoManager;
     }
 
-    private static final IBackendFactory getBackendFactory() {
-        if (backendFactory == null) {
-            backendFactory = new BackendFactory(getRuntimeInfoManager());
-        }
-        return backendFactory;
-    }
-
     public static final IBackendManager getBackendManager() {
         if (backendManager == null) {
             final RuntimeInfo erlideRuntime = getRuntimeInfoManager()
                     .getErlideRuntime();
-            backendManager = new BackendManager(erlideRuntime,
-                    getBackendFactory());
+            final BackendFactory backendFactory = new BackendFactory(
+                    getRuntimeInfoManager());
+            backendManager = new BackendManager(erlideRuntime, backendFactory);
         }
         return backendManager;
     }
-
 }
