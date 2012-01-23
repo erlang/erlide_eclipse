5b26dad3e908486c01c426a08039e3109579a548
Vlad Dumitrescu <vladdu55@gmail.com>
2012-01-23 15:42:44 +0100

    removed code in EditorUtility.revealInEditor that was never called

--- org.erlide.ui/src/org/erlide/ui/editors/util/EditorUtility.java
+++ org.erlide.ui/src/org/erlide/ui/editors/util/EditorUtility.java
@@ -12,7 +12,6 @@
 package org.erlide.ui.editors.util;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.List;
 
@@ -20,32 +19,23 @@ import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.ide.IGotoMarker;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.erlide.core.model.erlang.IErlModule;
@@ -184,66 +174,9 @@ public class EditorUtility {
             ((ITextEditor) editor).selectAndReveal(offset, length);
             return;
         }
-
-        // Support for non-text editor - try IGotoMarker interface
-        // FIXME is this ever used, ever?
-        if (editor instanceof IGotoMarker) {
-            final IEditorInput input = editor.getEditorInput();
-            if (input instanceof IFileEditorInput) {
-                final IGotoMarker gotoMarkerTarget = (IGotoMarker) editor;
-                final WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
-
-                    @Override
-                    protected void execute(final IProgressMonitor monitor)
-                            throws CoreException {
-                        IMarker marker = null;
-                        try {
-                            marker = ((IFileEditorInput) input).getFile()
-                                    .createMarker(IMarker.TEXT);
-                            marker.setAttribute(IMarker.CHAR_START, offset);
-                            marker.setAttribute(IMarker.CHAR_END, offset
-                                    + length);
-
-                            gotoMarkerTarget.gotoMarker(marker);
-
-                        } finally {
-                            if (marker != null) {
-                                marker.delete();
-                            }
-                        }
-                    }
-                };
-
-                try {
-                    op.run(null);
-                } catch (final InvocationTargetException ex) {
-                    // reveal failed
-                } catch (final InterruptedException e) {
-                    Assert.isTrue(false, "this operation can not be canceled"); //$NON-NLS-1$
-                }
-            }
-            return;
-        }
-
-        /*
-         * Workaround: send out a text selection XXX: Needs to be improved, see
-         * https://bugs.eclipse.org/bugs/show_bug.cgi?id=32214
-         */
-        if (editor != null
-                && editor.getEditorSite().getSelectionProvider() != null) {
-            final IEditorSite site = editor.getEditorSite();
-            if (site == null) {
-                return;
-            }
-
-            final ISelectionProvider provider = editor.getEditorSite()
-                    .getSelectionProvider();
-            if (provider == null) {
-                return;
-            }
-
-            provider.setSelection(new TextSelection(offset, length));
-        }
+        ErlLogger
+                .warn("EditorUtility.revealInEditor should only be called on an ErlangEditor; it was an %s",
+                        editor.getClass().getName());
     }
 
     private static IEditorPart openInEditor(final IFile file,
