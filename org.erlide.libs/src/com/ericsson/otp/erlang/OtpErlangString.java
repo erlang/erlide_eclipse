6e00a4391f5e8053aaa87f8df8d08b04a644e727
Vlad Dumitrescu <vladdu55@gmail.com>
2012-09-18 09:48:07 +0200

    update jinterface to R15

--- org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangString.java
+++ org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangString.java
@@ -1,7 +1,7 @@
 /*
  * %CopyrightBegin%
  * 
- * Copyright Ericsson AB 2000-2009. All Rights Reserved.
+ * Copyright Ericsson AB 2000-2012. All Rights Reserved.
  * 
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
@@ -168,7 +168,7 @@ public class OtpErlangString extends OtpErlangObject implements Serializable,
     /**
      * Validate a code point according to Erlang definition; Unicode 3.0. That
      * is; valid in the range U+0..U+10FFFF, but not in the range U+D800..U+DFFF
-     * (surrogat pairs), nor U+FFFE..U+FFFF (non-characters).
+     * (surrogat pairs).
      * 
      * @param cp
      *            the code point value to validate
@@ -180,8 +180,8 @@ public class OtpErlangString extends OtpErlangObject implements Serializable,
         // Erlang definition of valid Unicode code points;
         // Unicode 3.0, XML, et.al.
         return cp >>> 16 <= 0x10 // in 0..10FFFF; Unicode range
-                && (cp & ~0x7FF) != 0xD800 // not in D800..DFFF; surrogate range
-                && (cp & ~1) != 0xFFFE; // not in FFFE..FFFF; non-characters
+                && (cp & ~0x7FF) != 0xD800; // not in D800..DFFF; surrogate
+                                            // range
     }
 
     /**
