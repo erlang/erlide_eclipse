36e6c32e9b202421e90e55b93f96db7d49a092d5
Vlad Dumitrescu <vladdu55@gmail.com>
2013-02-13 08:35:30 +0100

    1143: Erlang application Run Configuration settings on Runtimes tab are blank after updating from 0.18 to 0.19

--- org.erlide.backend/src/org/erlide/launch/ErlLaunchAttributes.java
+++ org.erlide.backend/src/org/erlide/launch/ErlLaunchAttributes.java
@@ -18,43 +18,44 @@ import org.erlide.backend.BackendPlugin;
  */
 public interface ErlLaunchAttributes {
 
-    final String prefix = BackendPlugin.PLUGIN_ID;
+    final String NEW_PREFIX = BackendPlugin.PLUGIN_ID;
+    final String PREFIX = "org.erlide.core";
 
-    final String PROJECTS = prefix + ".projects";
+    final String PROJECTS = PREFIX + ".projects";
 
-    final String MODULE = prefix + ".module";
-    final String FUNCTION = prefix + ".function";
-    final String ARGUMENTS = prefix + ".arguments";
+    final String MODULE = PREFIX + ".module";
+    final String FUNCTION = PREFIX + ".function";
+    final String ARGUMENTS = PREFIX + ".arguments";
 
-    final String RUNTIME_NAME = prefix + ".runtime";
+    final String RUNTIME_NAME = PREFIX + ".runtime";
     final String DEFAULT_RUNTIME_NAME = "erts";
 
-    final String NODE_NAME = prefix + ".nodeName";
-    final String COOKIE = prefix + ".cookie";
-    final String START_ME = prefix + ".startMe";
-    final String USE_LONG_NAME = prefix + ".longName";
+    final String NODE_NAME = PREFIX + ".nodeName";
+    final String COOKIE = PREFIX + ".cookie";
+    final String START_ME = PREFIX + ".startMe";
+    final String USE_LONG_NAME = PREFIX + ".longName";
 
-    final String DEBUG_FLAGS = prefix + ".debugFlags"; // @see
+    final String DEBUG_FLAGS = PREFIX + ".debugFlags"; // @see
     // IErlDebugConstants
 
-    final String DEBUG_INTERPRET_MODULES = prefix + ".interpretModules";
+    final String DEBUG_INTERPRET_MODULES = PREFIX + ".interpretModules";
 
-    final String WORKING_DIR = prefix + ".working_dir";
+    final String WORKING_DIR = PREFIX + ".working_dir";
     final String DEFAULT_WORKING_DIR = ".";
 
-    final String EXTRA_ARGS = prefix + ".extra_args";
+    final String EXTRA_ARGS = PREFIX + ".extra_args";
 
-    final String CONSOLE = prefix + ".console";
-    final String INTERNAL = prefix + ".has_backend";
+    final String CONSOLE = PREFIX + ".console";
+    final String INTERNAL = PREFIX + ".has_backend";
 
     final String DEBUG_TRACED_MODULES = ".traced_modules";
 
-    final String LOAD_ALL_NODES = prefix + ".loadAllNodes";
+    final String LOAD_ALL_NODES = PREFIX + ".loadAllNodes";
 
-    final String RESTARTABLE = prefix + ".restartable";
+    final String RESTARTABLE = PREFIX + ".restartable";
 
-    final String MANAGED = prefix + ".managed";
+    final String MANAGED = PREFIX + ".managed";
 
-    final String SHELL = prefix + ".startShell";
+    final String SHELL = PREFIX + ".startShell";
 
 }
