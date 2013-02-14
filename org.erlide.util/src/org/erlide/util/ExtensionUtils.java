45aa8118d685326b8107420ade63b06739839d74
Vlad Dumitrescu <vladdu55@gmail.com>
2013-02-14 16:16:12 +0100

    add Extensionutils.getSingletonProviderExtension

--- org.erlide.util/src/org/erlide/util/ExtensionUtils.java
+++ org.erlide.util/src/org/erlide/util/ExtensionUtils.java
@@ -1,5 +1,9 @@
 package org.erlide.util;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
@@ -16,7 +20,7 @@ public class ExtensionUtils {
      * checks, but take the first one.
      */
     public static <T> T getSingletonExtension(final String id,
-            final Class<T> clazz) {
+            final Class<? extends T> clazz) {
         final IExtensionRegistry reg = RegistryFactory.getRegistry();
         final IConfigurationElement[] elements = reg
                 .getConfigurationElementsFor(id);
@@ -32,6 +36,53 @@ public class ExtensionUtils {
             }
         }
         return null;
+    }
+
+    public static <T> Provider<? extends T> getSingletonProviderExtension(
+            final String id, final Class<? extends Provider<? extends T>> clazz) {
+        final IExtensionRegistry reg = RegistryFactory.getRegistry();
+        final IConfigurationElement[] elements = reg
+                .getConfigurationElementsFor(id);
+        for (final IConfigurationElement element : elements) {
+            try {
+                final Object object = element
+                        .createExecutableExtension("provider");
+                if (clazz.isInstance(object)) {
+                    return clazz.cast(object);
+                }
+            } catch (final CoreException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the underlying class for a type, or null if the type is a variable
+     * type.
+     * 
+     * @param type
+     *            the type
+     * @return the underlying class
+     */
+    @SuppressWarnings("rawtypes")
+    public static Class<?> getClass(final Type type) {
+        if (type instanceof Class) {
+            return (Class) type;
+        } else if (type instanceof ParameterizedType) {
+            return getClass(((ParameterizedType) type).getRawType());
+        } else if (type instanceof GenericArrayType) {
+            final Type componentType = ((GenericArrayType) type)
+                    .getGenericComponentType();
+            final Class<?> componentClass = getClass(componentType);
+            if (componentClass != null) {
+                return Array.newInstance(componentClass, 0).getClass();
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
     public static <T> List<T> getExtensions(final String id,
