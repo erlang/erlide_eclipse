eb9af047b715f31009ad33198ae9eedeaa396021
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 21:01:33 +0100

    jinterface: fix array comparisons in OtpErlangFun.equals

--- org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangFun.java
+++ org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangFun.java
@@ -1,24 +1,25 @@
 /*
  * %CopyrightBegin%
- * 
+ *
  * Copyright Ericsson AB 2009. All Rights Reserved.
- * 
+ *
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
  * compliance with the License. You should have received a copy of the
  * Erlang Public License along with this software. If not, it can be
  * retrieved online at http://www.erlang.org/.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
- * %CopyrightEnd% 
+ *
+ * %CopyrightEnd%
  */
 package com.ericsson.otp.erlang;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class OtpErlangFun extends OtpErlangObject implements Serializable {
     // don't change this!
@@ -91,7 +92,7 @@ public class OtpErlangFun extends OtpErlangObject implements Serializable {
                 return false;
             }
         } else {
-            if (!md5.equals(f.md5)) {
+            if (!Arrays.equals(md5, f.md5)) {
                 return false;
             }
         }
@@ -101,7 +102,7 @@ public class OtpErlangFun extends OtpErlangObject implements Serializable {
         if (freeVars == null) {
             return f.freeVars == null;
         }
-        return freeVars.equals(f.freeVars);
+        return Arrays.equals(freeVars, f.freeVars);
     }
 
     @Override
