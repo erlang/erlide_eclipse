8bd1fc126ac0e9cb81f2995db5c6bd421149d654
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 21:30:04 +0100

    jinterface: OtpErlangList.clone must not return null

--- org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangList.java
+++ org.erlide.libs/src/com/ericsson/otp/erlang/OtpErlangList.java
@@ -1,19 +1,19 @@
 /*
  * %CopyrightBegin%
- * 
+ *
  * Copyright Ericsson AB 2000-2009. All Rights Reserved.
- * 
+ *
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
  * compliance with the License. You should have received a copy of the
  * Erlang Public License along with this software. If not, it can be
  * retrieved online at http://www.erlang.org/.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * %CopyrightEnd%
  */
 package com.ericsson.otp.erlang;
@@ -324,9 +324,11 @@ public class OtpErlangList extends OtpErlangObject implements
     @Override
     public Object clone() {
         try {
-            return new OtpErlangList(elements(), getLastTail());
+            return new OtpErlangList(elements(),
+                    elements().length != 0 ? getLastTail() : null);
         } catch (final OtpErlangException e) {
-            return null;
+            // can never happen
+            throw new AssertionError();
         }
     }
 
