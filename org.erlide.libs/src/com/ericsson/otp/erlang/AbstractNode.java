57f9a11aa525044b44a85c396ec471db29e6ebc0
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 20:58:30 +0100

    update jinterface to R16

--- org.erlide.libs/src/com/ericsson/otp/erlang/AbstractNode.java
+++ org.erlide.libs/src/com/ericsson/otp/erlang/AbstractNode.java
@@ -1,20 +1,20 @@
 /*
  * %CopyrightBegin%
- *
- * Copyright Ericsson AB 2000-2009. All Rights Reserved.
- *
+ * 
+ * Copyright Ericsson AB 2000-2013. All Rights Reserved.
+ * 
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
  * compliance with the License. You should have received a copy of the
  * Erlang Public License along with this software. If not, it can be
  * retrieved online at http://www.erlang.org/.
- *
+ * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- *
- * %CopyrightEnd%
+ * 
+ * %CopyrightEnd% 
  */
 package com.ericsson.otp.erlang;
 
@@ -254,14 +254,13 @@ public class AbstractNode {
     }
 
     private static String getHomeDir() {
-        final String u = System.getProperty("user.home");
+        final String home = System.getProperty("user.home");
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            final String d = System.getenv("HOMEDRIVE");
-            final String p = System.getenv("HOMEPATH");
-            return (d != null && p != null) ? d + p : u;
+            final String drive = System.getenv("HOMEDRIVE");
+            final String path = System.getenv("HOMEPATH");
+            return drive != null && path != null ? drive + path : home;
         } else {
-            return u;
+            return home;
         }
     }
-
 }
