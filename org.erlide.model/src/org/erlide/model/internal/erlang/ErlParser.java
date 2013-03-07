e63081d7f0e28a2d1f7413d860dc7055f9c4280a
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 21:51:29 +0100

    small fix

--- org.erlide.model/src/org/erlide/model/internal/erlang/ErlParser.java
+++ org.erlide.model/src/org/erlide/model/internal/erlang/ErlParser.java
@@ -51,7 +51,7 @@ import com.google.common.collect.Lists;
  */
 public final class ErlParser implements IErlParser {
 
-    private final class SourceOffsetComparator implements
+    private static final class SourceOffsetComparator implements
             Comparator<ISourceReference> {
         @Override
         public int compare(final ISourceReference o1, final ISourceReference o2) {
