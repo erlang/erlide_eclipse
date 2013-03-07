372addf358168d080389646b874391d4903a3014
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 21:03:46 +0100

    jinterface: fix NPE when .erlang.cookie file is empty

--- org.erlide.libs/src/com/ericsson/otp/erlang/AbstractNode.java
+++ org.erlide.libs/src/com/ericsson/otp/erlang/AbstractNode.java
@@ -1,20 +1,20 @@
 /*
  * %CopyrightBegin%
- * 
+ *
  * Copyright Ericsson AB 2000-2013. All Rights Reserved.
- * 
+ *
  * The contents of this file are subject to the Erlang Public License,
  * Version 1.1, (the "License"); you may not use this file except in
  * compliance with the License. You should have received a copy of the
  * Erlang Public License along with this software. If not, it can be
  * retrieved online at http://www.erlang.org/.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
- * %CopyrightEnd% 
+ *
+ * %CopyrightEnd%
  */
 package com.ericsson.otp.erlang;
 
@@ -127,7 +127,8 @@ public class AbstractNode {
             final File dotCookieFile = new File(dotCookieFilename);
 
             br = new BufferedReader(new FileReader(dotCookieFile));
-            defaultCookie = br.readLine().trim();
+            final String line = br.readLine();
+            defaultCookie = line != null ? line.trim() : "";
         } catch (final IOException e) {
             defaultCookie = "";
         } finally {
