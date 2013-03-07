19b54d56ee0b53c7554b68ae2935d7a29738d179
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-07 10:53:47 +0100

    add test for 1158

---
+++ org.erlide.model.tests/src/org/erlide/model/util/PathSerializerTest.java
@@ -0,0 +1,24 @@
+package org.erlide.model.util;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.erlide.model.internal.root.PathSerializer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class PathSerializerTest {
+
+    @Test
+    public void shouldFilterOutEmptyPaths() {
+        final String input = ";a;b;;c;;;d;";
+        final Collection<IPath> expected = Lists.newArrayList((IPath) new Path(
+                "a"), (IPath) new Path("b"), (IPath) new Path("c"),
+                (IPath) new Path("d"));
+        final Collection<IPath> actual = PathSerializer.unpackList(input);
+        Assert.assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+}
