476b7792a70442e73355b26d7214995f96f934ca
Vlad Dumitrescu <vladdu55@gmail.com>
2013-03-11 13:34:15 +0100

    1165 - crash when starting and no runtime is found

--- org.erlide.runtime/src/org/erlide/runtime/RuntimeCore.java
+++ org.erlide.runtime/src/org/erlide/runtime/RuntimeCore.java
@@ -2,6 +2,7 @@ package org.erlide.runtime;
 
 import org.erlide.runtime.runtimeinfo.IRuntimeInfoCatalog;
 import org.erlide.runtime.runtimeinfo.IRuntimeInfoSerializer;
+import org.erlide.runtime.runtimeinfo.RuntimeInfo;
 import org.erlide.runtime.runtimeinfo.RuntimeInfoCatalog;
 import org.erlide.runtime.runtimeinfo.RuntimeInfoCatalogData;
 import org.erlide.util.HostnameUtils;
@@ -18,8 +19,10 @@ public class RuntimeCore {
             runtimeInfoCatalog = new RuntimeInfoCatalog();
             runtimeInfoCatalog.setRuntimes(data.runtimes,
                     data.defaultRuntimeName, data.erlideRuntimeName);
-            HostnameUtils.detectHostNames(runtimeInfoCatalog.erlideRuntime
-                    .getOtpHome());
+            final RuntimeInfo erlideRuntime = runtimeInfoCatalog.erlideRuntime;
+            if (erlideRuntime != null) {
+                HostnameUtils.detectHostNames(erlideRuntime.getOtpHome());
+            }
         }
         return runtimeInfoCatalog;
     }
