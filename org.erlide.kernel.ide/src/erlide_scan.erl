7ecd995cdd5a4f6f316de8304dd874f844ada0a8
Vlad Dumitrescu <vladdu55@gmail.com>
2013-06-05 22:35:07 +0200

    fix for scanning $\n, was incrementing the row too

--- org.erlide.kernel.ide/src/erlide_scan.erl
+++ org.erlide.kernel.ide/src/erlide_scan.erl
@@ -423,7 +423,7 @@ sub_scan_escape([$\n|Cs], Pos) ->
 sub_scan_escape([$n=C|Cs], Pos) ->
     Val = escape_char(C),
     StrVal = [C],
-    {Cs, Val, StrVal, incrow(Pos)};
+    {Cs, Val, StrVal, inc(Pos, 1)};
 sub_scan_escape([C|Cs], Pos) ->
     Val = escape_char(C),
     StrVal = [C],
