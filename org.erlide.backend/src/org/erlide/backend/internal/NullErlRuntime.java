6fb97b37219f4975347dc625e7234049410f35f5
Vlad Dumitrescu <vladdu55@gmail.com>
2013-06-17 10:33:18 +0200

    NullErlRuntime should not implement IRpcSite

--- org.erlide.backend/src/org/erlide/backend/internal/NullErlRuntime.java
+++ org.erlide.backend/src/org/erlide/backend/internal/NullErlRuntime.java
@@ -5,19 +5,13 @@ import org.erlide.runtime.api.IErlRuntime;
 import org.erlide.runtime.api.IRpcSite;
 import org.erlide.runtime.api.IRuntimeStateListener;
 import org.erlide.runtime.api.RuntimeData;
-import org.erlide.runtime.rpc.IRpcCallback;
-import org.erlide.runtime.rpc.IRpcFuture;
-import org.erlide.runtime.rpc.IRpcResultCallback;
-import org.erlide.runtime.rpc.RpcException;
-import org.erlide.runtime.rpc.RpcResult;
 import org.erlide.runtime.shell.IBackendShell;
 import org.erlide.util.ExtensionHelper;
 
-import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangPid;
 import com.ericsson.otp.erlang.OtpMbox;
 
-public class NullErlRuntime implements IErlRuntime, IRpcSite {
+public class NullErlRuntime implements IErlRuntime {
 
     private void reportNoBackend() {
         final Runnable handler = (Runnable) ExtensionHelper
@@ -25,124 +19,6 @@ public class NullErlRuntime implements IErlRuntime, IRpcSite {
         if (handler != null) {
             handler.run();
         }
-    }
-
-    @Override
-    public RpcResult call_noexception(final String m, final String f,
-            final String signature, final Object... a) {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public RpcResult call_noexception(final long timeout, final String m,
-            final String f, final String signature, final Object... args) {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public IRpcFuture async_call(final String m, final String f,
-            final String signature, final Object... args) throws RpcException {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public IRpcFuture async_call(final OtpErlangObject gleader, final String m,
-            final String f, final String signature, final Object... args)
-            throws RpcException {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public void async_call_cb(final IRpcCallback cb, final String m,
-            final String f, final String signature, final Object... args)
-            throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void async_call_cb(final IRpcCallback cb, final long timeout,
-            final String m, final String f, final String signature,
-            final Object... args) throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void async_call_cb(final IRpcCallback cb, final long timeout,
-            final OtpErlangObject gleader, final String m, final String f,
-            final String signature, final Object... args) throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void cast(final String m, final String f, final String signature,
-            final Object... args) throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void cast(final OtpErlangObject gleader, final String m,
-            final String f, final String signature, final Object... args)
-            throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public OtpErlangObject call(final String m, final String f,
-            final String signature, final Object... a) throws RpcException {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public OtpErlangObject call(final long timeout, final String m,
-            final String f, final String signature, final Object... a)
-            throws RpcException {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public OtpErlangObject call(final long timeout,
-            final OtpErlangObject gleader, final String m, final String f,
-            final String signature, final Object... a) throws RpcException {
-        reportNoBackend();
-        return null;
-    }
-
-    @Override
-    public void async_call_result(final IRpcResultCallback cb, final String m,
-            final String f, final String signature, final Object... args)
-            throws RpcException {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void send(final OtpErlangPid pid, final Object msg) {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void send(final String name, final Object msg) {
-        reportNoBackend();
-
-    }
-
-    @Override
-    public void send(final String fullNodeName, final String name,
-            final Object msg) {
-        reportNoBackend();
-
     }
 
     @Override
@@ -250,13 +126,6 @@ public class NullErlRuntime implements IErlRuntime, IRpcSite {
 
     @Override
     public void tryConnect() {
-    }
-
-    @Override
-    public OtpErlangObject getRpcResult(final OtpMbox mbox, final long timeout,
-            final String env) throws RpcException {
-        reportNoBackend();
-        return null;
     }
 
     @Override
