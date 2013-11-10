b34030246ba46c436af62a1bc181c1e2ee2d89c5
Vlad Dumitrescu <vladdu55@gmail.com>
2013-11-10 22:58:33 +0100

    builder: internal props change listeners

--- org.erlide.model.tests/src/org/erlide/engine/model/erlang/configuration/InternalProjectConfigurationTests.java
+++ org.erlide.model.tests/src/org/erlide/engine/model/erlang/configuration/InternalProjectConfigurationTests.java
@@ -9,20 +9,24 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.erlide.engine.model.root.ErlangProjectProperties;
 import org.erlide.engine.model.root.ProjectPreferencesConstants;
 import org.junit.Test;
+import org.osgi.service.prefs.BackingStoreException;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 
-public class InternalProjectConfigurationTests extends AbstractProjectConfigurationTests {
+public class InternalProjectConfigurationTests extends
+        AbstractProjectConfigurationTests {
 
     @Test
-    public void outputPathShouldFollowPropertyChange() {
+    public void outputPathShouldFollowPropertyChange()
+            throws BackingStoreException {
         final String expected = "hello/world";
 
         final IProject wproject = project.getWorkspaceProject();
         final IEclipsePreferences node = new ProjectScope(wproject)
                 .getNode("org.erlide.core");
         node.put(ProjectPreferencesConstants.OUTPUT_DIR, expected);
+        node.flush();
 
         final ErlangProjectProperties pp = project.getProperties();
         final String actual = pp.getOutputDir().toPortableString();
@@ -31,13 +35,15 @@ public class InternalProjectConfigurationTests extends AbstractProjectConfigurat
     }
 
     @Test
-    public void includePathsShouldFollowPropertyChange() {
+    public void includePathsShouldFollowPropertyChange()
+            throws BackingStoreException {
         final String expected = "hello/world;a/b";
 
         final IProject wproject = project.getWorkspaceProject();
         final IEclipsePreferences node = new ProjectScope(wproject)
                 .getNode("org.erlide.core");
         node.put(ProjectPreferencesConstants.INCLUDE_DIRS, expected);
+        node.flush();
 
         final ErlangProjectProperties pp = project.getProperties();
         final String actual = pp.getIncludeDirs().toString();
@@ -46,17 +52,20 @@ public class InternalProjectConfigurationTests extends AbstractProjectConfigurat
     }
 
     private String convertListString(final String expected) {
-        return "[" + Joiner.on(", ").join(Splitter.on(";").split(expected)) + "]";
+        return "[" + Joiner.on(", ").join(Splitter.on(";").split(expected))
+                + "]";
     }
 
     @Test
-    public void sourcePathsShouldFollowPropertyChange() {
+    public void sourcePathsShouldFollowPropertyChange()
+            throws BackingStoreException {
         final String expected = "hello/world;a/b";
 
         final IProject wproject = project.getWorkspaceProject();
         final IEclipsePreferences node = new ProjectScope(wproject)
                 .getNode("org.erlide.core");
         node.put(ProjectPreferencesConstants.SOURCE_DIRS, expected);
+        node.flush();
 
         final ErlangProjectProperties pp = project.getProperties();
         final String actual = pp.getSourceDirs().toString();
