bc0457bd5dbbfffc737989ae77fb7187b67fd4ad
Vlad Dumitrescu <vladdu55@gmail.com>
2014-01-29 16:09:34 +0100

    start reimplementing project properties page

--- org.erlide.ui/src/org/erlide/ui/properties/ErlangProjectPropertyPage.java
+++ org.erlide.ui/src/org/erlide/ui/properties/ErlangProjectPropertyPage.java
@@ -19,19 +19,27 @@ import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
 import org.erlide.engine.ErlangEngine;
 import org.erlide.engine.model.root.ErlangProjectProperties;
 import org.erlide.engine.model.root.IErlProject;
+import org.erlide.util.SystemConfiguration;
 
 public class ErlangProjectPropertyPage extends PropertyPage {
 
     private final ErlangProjectProperties model;
     private Text text;
     private Text text_1;
+    private Text text_2;
+    private Text text_3;
+    private Combo combo;
+    private Text text_4;
+    private Text text_5;
 
     public ErlangProjectPropertyPage() {
         super();
@@ -111,14 +119,96 @@ public class ErlangProjectPropertyPage extends PropertyPage {
     @Override
     protected Control createContents(final Composite parent) {
         final Composite composite = new Composite(parent, SWT.NONE);
-        composite.setLayout(new GridLayout(1, false));
+        composite.setLayout(new GridLayout(2, false));
+        {
+            final Label lblRequiredErlangVersion = new Label(composite, SWT.NONE);
+            lblRequiredErlangVersion.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER,
+                    false, false, 1, 1));
+            lblRequiredErlangVersion.setText("Required Erlang version");
+        }
+        {
+            combo = new Combo(composite, SWT.NONE);
+            final GridData gd_combo = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1,
+                    1);
+            gd_combo.widthHint = 83;
+            combo.setLayoutData(gd_combo);
+        }
+        {
+            new Label(composite, SWT.NONE);
+        }
+        new Label(composite, SWT.NONE);
+        {
+            final Label lblNewLabel = new Label(composite, SWT.NONE);
+            lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
+                    1, 1));
+            lblNewLabel.setText("Output directory");
+        }
         {
             text = new Text(composite, SWT.BORDER);
             text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
         }
         {
+            final Label lblNewLabel = new Label(composite, SWT.NONE);
+            lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
+                    1, 1));
+            lblNewLabel.setText("Source directories");
+        }
+        {
             text_1 = new Text(composite, SWT.BORDER);
             text_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        }
+        {
+            final Label lblNewLabel = new Label(composite, SWT.NONE);
+            lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
+                    1, 1));
+            lblNewLabel.setText("Include directories");
+        }
+        {
+            text_2 = new Text(composite, SWT.BORDER);
+            text_2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        }
+        {
+            final Label lblNewLabel_3 = new Label(composite, SWT.NONE);
+            lblNewLabel_3.setEnabled(false);
+            lblNewLabel_3.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
+                    1, 1));
+            lblNewLabel_3.setText("Test directories");
+        }
+        {
+            text_3 = new Text(composite, SWT.BORDER);
+            text_3.setEnabled(false);
+            text_3.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        }
+        new Label(composite, SWT.NONE);
+        new Label(composite, SWT.NONE);
+        {
+            final Label lblExternalModules = new Label(composite, SWT.NONE);
+            lblExternalModules.setVisible(true);
+            lblExternalModules.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
+                    false, 1, 1));
+            lblExternalModules.setText("External modules");
+            lblExternalModules
+                    .setEnabled(SystemConfiguration.getInstance().isDeveloper());
+        }
+        {
+            text_4 = new Text(composite, SWT.BORDER);
+            text_4.setEditable(false);
+            text_4.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            text_4.setEnabled(SystemConfiguration.getInstance().isDeveloper());
+        }
+        {
+            final Label lblExternalIncludes = new Label(composite, SWT.NONE);
+            lblExternalIncludes.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
+                    false, 1, 1));
+            lblExternalIncludes.setText("External includes");
+            lblExternalIncludes.setEnabled(SystemConfiguration.getInstance()
+                    .isDeveloper());
+        }
+        {
+            text_5 = new Text(composite, SWT.BORDER);
+            text_5.setEditable(false);
+            text_5.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            text_5.setEnabled(SystemConfiguration.getInstance().isDeveloper());
         }
         initDataBindings();
         return composite;
