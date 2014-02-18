970dc3cb1a0f154ed0814fc956340b6f9c5f74d2
Vlad Dumitrescu <vladdu55@gmail.com>
2014-02-18 09:22:20 +0100

    add getBinary and getAsString to Bindings

--- org.erlide.util/src/org/erlide/util/erlang/Bindings.java
+++ org.erlide.util/src/org/erlide/util/erlang/Bindings.java
@@ -3,6 +3,7 @@ package org.erlide.util.erlang;
 import java.util.Collection;
 import java.util.Map;
 
+import com.ericsson.otp.erlang.OtpErlangBinary;
 import com.ericsson.otp.erlang.OtpErlangException;
 import com.ericsson.otp.erlang.OtpErlangObject;
 import com.ericsson.otp.erlang.OtpErlangPid;
@@ -20,6 +21,10 @@ public interface Bindings {
     String getQuotedAtom(final String name) throws OtpErlangException;
 
     String getString(final String name) throws OtpErlangException;
+
+    OtpErlangBinary getBinary(String string) throws OtpErlangException;
+
+    String getAsString(final String name);
 
     Collection<OtpErlangObject> getList(final String name) throws OtpErlangException;
 
