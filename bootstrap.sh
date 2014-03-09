2dd5e731f13bb215854a8f6907828fce3dca46ed
Vlad Dumitrescu <vladdu55@gmail.com>
2014-03-09 19:35:51 +0100

    update vagrant config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,9 @@
 
 sudo apt-get update
 sudo apt-mark hold grub2 grub2-common grub-pc grub-pc-bin
-#sudo apt-get upgrade -y
-sudo apt-get install -y erlang ant openjdk-6-jdk
-
-set +x
+sudo apt-get upgrade -y
+sudo apt-get install -y erlang ant openjdk-6-jdk git ruby1.9.3 jruby
+sudo apt-get autoremove
 
 rm -rf $HOME/erlide
 ln -s /vagrant $HOME/erlide
