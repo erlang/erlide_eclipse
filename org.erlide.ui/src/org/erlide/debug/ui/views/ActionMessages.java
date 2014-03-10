3727d1da7d652e0ef6b2d3965d9bd5da5afed52c
Vlad Dumitrescu <vladdu55@gmail.com>
2014-03-10 10:41:18 +0100

    remove unused code

--- org.erlide.ui/src/org/erlide/debug/ui/views/ActionMessages.java
+++ org.erlide.ui/src/org/erlide/debug/ui/views/ActionMessages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public final class ActionMessages {
-    private static final String BUNDLE_NAME = "org.erlide.debug.ui.views.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.erlide.debug.ui.views.actionmessages"; //$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
             .getBundle(BUNDLE_NAME);
