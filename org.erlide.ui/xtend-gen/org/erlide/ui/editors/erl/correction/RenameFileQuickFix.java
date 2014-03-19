0547533b75ddddfe893a547696c254f5903e4047
Vlad Dumitrescu <vladdu55@gmail.com>
2014-03-19 16:02:39 +0100

    add quickfix extension point

---
+++ org.erlide.ui/xtend-gen/org/erlide/ui/editors/erl/correction/RenameFileQuickFix.java
@@ -0,0 +1,12 @@
+package org.erlide.ui.editors.erl.correction;
+
+import org.eclipse.swt.graphics.Image;
+
+public class RenameFileQuickFix extends ErlangQuickFix {
+
+    public RenameFileQuickFix(String label, String description, Image image) {
+        super(label, description, image);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ org.erlide.ui/xtend-gen/org/erlide/ui/editors/erl/correction/RenameModuleQuickFix.java
@@ -0,0 +1,12 @@
+package org.erlide.ui.editors.erl.correction;
+
+import org.eclipse.swt.graphics.Image;
+
+public class RenameModuleQuickFix extends ErlangQuickFix {
+
+    public RenameModuleQuickFix(String label, String description, Image image) {
+        super(label, description, image);
+        // TODO Auto-generated constructor stub
+    }
+
+}
