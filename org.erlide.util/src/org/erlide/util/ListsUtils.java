e30948a053619d6cb8b047dee552e145c859dfbc
Vlad Dumitrescu <vladdu55@gmail.com>
2014-03-31 11:23:02 +0200

    simplify ListUtils

--- org.erlide.util/src/org/erlide/util/ListsUtils.java
+++ org.erlide.util/src/org/erlide/util/ListsUtils.java
@@ -2,43 +2,28 @@ package org.erlide.util;
 
 import java.util.List;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 
 public class ListsUtils {
-    private static final List<String> EMPTY_LIST = Lists.newArrayList();
-
     public static String packList(final Iterable<String> strs, final String sep) {
-        final StringBuilder result = new StringBuilder();
-        for (final String s : strs) {
-            result.append(s).append(sep);
-        }
-        return result.length() > 0 ? result.substring(0, result.length() - sep.length())
-                : "";
+        return Joiner.on(sep).join(strs);
     }
 
     public static String packArray(final String[] strs, final String sep) {
-        final StringBuilder result = new StringBuilder();
-        for (final String s : strs) {
-            result.append(s).append(sep);
-        }
-        return result.length() > 0 ? result.substring(0, result.length() - sep.length())
-                : "";
+        return Joiner.on(sep).join(strs);
     }
 
     public static String[] unpackArray(final String str, final String sep) {
-        return ListsUtils.unpackList(str, sep).toArray(new String[0]);
+        return unpackList(str, sep).toArray(new String[0]);
     }
 
     public static List<String> unpackList(final String string, final String sep) {
         if (string.length() == 0) {
-            return EMPTY_LIST;
+            return Lists.newArrayList();
         }
-        final String[] v = string.split(sep);
-        final List<String> result = Lists.newArrayListWithCapacity(v.length);
-        for (final String s : v) {
-            result.add(s);
-        }
-        return result;
+        return Lists.newArrayList(Splitter.on(sep).split(string));
     }
 
 }
