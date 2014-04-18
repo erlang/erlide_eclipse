c44bc687a41d2908063b6156d9322bc04e825be9
Vlad Dumitrescu <vladdu55@gmail.com>
2014-04-18 23:40:39 +0200

    fix setup_tools.sh

--- org.erlide.releng/setup_tools.sh
+++ org.erlide.releng/setup_tools.sh
@@ -2,24 +2,31 @@
 
 # install the required things for building erlide
 #
-# run as user who will run (jenkins)
+# run as user who will run the builds (jenkins)
 #
-# the tools are installed in ~/erlide_tools
-#
-# jdk 1.6 install requires pressing "enter" once
+# script must be run from user's home
+# the tools are installed in erlide_tools
 
-mkdir -p erlide_tools
+ROOT_DIR=$1
+
+if [ -z "$ROOT_DIR" ]
+  then
+    echo "Specify root dir as argument!"
+    exit 1
+fi
+
+mkdir -p $ROOT_DIR/erlide_tools
 pushd erlide_tools
 
 # jdk 1.6
 if [ ! -d jdk ] ; then
-  if [ ! -f jdk-6u26-linux-i586.bin ] ; then
-      wget http://download.erlide.org/tools/jdk-6u26-linux-i586.bin
+  if [ ! -f jdk-6u45-linux-x64.bin ] ; then
+      wget http://download.erlide.org/tools/jdk-6u45-linux-x64.bin
   fi
-  chmod u+x jdk-6u26-linux-i586.bin
-  ./jdk-6u26-linux-i586.bin
-  ln -s jdk1.6.0_26 jdk
-  rm jdk-6u26-linux-i586.bin
+  chmod u+x jdk-6u45-linux-x64.bin
+  ./jdk-6u45-linux-x64.bin
+  ln -s jdk1.6.0_45 jdk
+  rm jdk-6u45-linux-x64.bin
 fi
 
 # ant 1.9.2
@@ -32,37 +39,20 @@ if [ ! -d ant ] ; then
   rm apache-ant-1.9.2-bin.tar.gz
 fi
 
-# jruby 1.7.4
+# jruby 1.7
 if [ ! -d jruby ] ; then
-  if [ ! -f jruby-bin-1.7.4.tar.gz ] ; then
-    wget http://download.erlide.org/tools/jruby-bin-1.7.4.tar.gz
+  if [ ! -f jruby-bin-1.7.12.tar.gz ] ; then
+    wget http://download.erlide.org/tools/jruby-bin-1.7.12.tar.gz
   fi
-  tar zxvf jruby-bin-1.7.4.tar.gz
-  ln -s jruby-1.7.4 jruby
-  rm jruby-bin-1.7.4.tar.gz
+  tar zxvf jruby-bin-1.7.12.tar.gz
+  ln -s jruby-1.7.12 jruby
+  rm jruby-bin-1.7.12.tar.gz
 
   jruby/bin/jruby -S gem install rake
 fi
 
 sudo apt-get update
-sudo apt-get install -y build-essential make perl gcc sed m4 ncurses-dev
-
-# erlang R14
-if [ ! -d otp ] ; then
-  if [ ! -f otp_src_R14B04.tar.gz ] ; then
-    wget http://download.erlide.org/tools/otp_src_R14B04.tar.gz
-  fi
-  tar zxvf otp_src_R14B04.tar.gz
-  rm otp_src_R14B04.tar.gz
-
-  pushd otp_src_R14B04
-  export LANG=C
-  ./configure
-  make
-  popd
-
-  ln -s otp_src_R14B04 otp
-fi
+sudo apt-get install -y build-essential make perl gcc sed m4 libncurses5-dev ncurses-dev libssl-dev autoconf
 
 # erlang R15
 if [ ! -d otp15 ] ; then
@@ -114,15 +104,12 @@ if [ ! -d otp17 ] ; then
   ln -s otp_src_17.0 otp17
 fi
 
+ln -s otp15 otp
+
 # TODO install eclipse target
 
 echo "add links to path"
-mkdir bin
-ln -s `pwd`/ant/bin/ant bin/ant
-ln -s `pwd`/jruby/bin/jruby bin/jruby
-
-export PATH=`pwd`/bin:$PATH
-echo "export PATH=$PATH"
+export PATH=`pwd`/ant/bin:`pwd`/jruby/bin:$PATH
+echo "export PATH=$PATH" >> $ROOT_DIR/.bashrc
 
 popd
-
