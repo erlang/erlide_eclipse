098a087a1a47d166e3e090b5d421b559878164f5
Vlad Dumitrescu <vladdu55@gmail.com>
2014-07-10 10:27:24 +0200

    remove unused classes

--- org.erlide.util/src/org/erlide/util/services/ExtensionUtils.java
+++ org.erlide.util/src/org/erlide/util/services/ExtensionUtils.java
@@ -5,10 +5,14 @@ import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.RegistryFactory;
 import org.erlide.util.ErlLogger;
 
@@ -103,6 +107,78 @@ public final class ExtensionUtils {
             }
         }
         return result;
+    }
+
+    /**
+     * This should be used to add participants at test-time. It should be the
+     * name of the extension point to a list (which will be returned)
+     */
+    public static Map<String, List<Object>> testingParticipants;
+
+    public static IExtension[] getExtensions(final String type) {
+        IExtension[] extensions;
+        final IExtensionRegistry registry = Platform.getExtensionRegistry();
+        // we may not be in eclipse env when testing
+        if (registry != null) {
+            try {
+                final IExtensionPoint extensionPoint = registry.getExtensionPoint(type);
+                extensions = extensionPoint.getExtensions();
+            } catch (final Exception e) {
+                ErlLogger.error("Error getting extension for:" + type + " -- "
+                        + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        } else {
+            extensions = new IExtension[0];
+        }
+        return extensions;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Object getParticipant(final String type) {
+        // only one participant may be used for this
+        final List<Object> participants = (List<Object>) getParticipants(type);
+        if (participants.size() == 1) {
+            return participants.get(0);
+        }
+        if (participants.isEmpty()) {
+            return null;
+        }
+        if (participants.size() > 1) {
+            throw new RuntimeException(
+                    "More than one participant is registered for type:" + type);
+        }
+        throw new RuntimeException("Should never get here!");
+    }
+
+    /**
+     * @param type
+     *            the extension we want to get
+     * @return a list of classes created from those extensions
+     */
+    public static List<?> getParticipants(final String type) {
+        if (testingParticipants != null) {
+            return testingParticipants.get(type);
+        }
+
+        final List<Object> list = Lists.newArrayList();
+        final IExtension[] extensions = getExtensions(type);
+        // For each extension ...
+        for (int i = 0; i < extensions.length; i++) {
+            final IExtension extension = extensions[i];
+            final IConfigurationElement[] elements = extension.getConfigurationElements();
+            // For each member of the extension ...
+            for (int j = 0; j < elements.length; j++) {
+                final IConfigurationElement element = elements[j];
+
+                try {
+                    list.add(element.createExecutableExtension("class"));
+                } catch (final Exception e) {
+                    ErlLogger.warn(e);
+                }
+            }
+        }
+        return list;
     }
 
     private ExtensionUtils() {
