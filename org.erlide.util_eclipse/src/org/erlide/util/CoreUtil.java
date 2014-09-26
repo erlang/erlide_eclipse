05a08b6e1627ffa0d35174c653317f6ff1dd32f2
Vlad Dumitrescu <vladdu55@gmail.com>
2014-09-26 10:48:32 +0200

    remove unused code in Util and CoreUtil

--- org.erlide.util_eclipse/src/org/erlide/util/CoreUtil.java
+++ org.erlide.util_eclipse/src/org/erlide/util/CoreUtil.java
@@ -10,72 +10,9 @@
  *******************************************************************************/
 package org.erlide.util;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 
 public final class CoreUtil {
-    /**
-     * Returns the given file's contents as a byte array.
-     *
-     * @throws CoreException
-     * @throws IOException
-     */
-    public static byte[] getResourceContentsAsByteArray(final IFile file)
-            throws CoreException, IOException {
-        InputStream stream = null;
-        stream = new BufferedInputStream(file.getContents(true));
-        try {
-            return Util.getInputStreamAsByteArray(stream, -1);
-        } finally {
-            try {
-                stream.close();
-            } catch (final IOException e) {
-                // ignore
-            }
-        }
-    }
-
-    /**
-     * Returns the given file's contents as a character array.
-     *
-     * @throws CoreException
-     * @throws IOException
-     *
-     * @throws ErlModelException
-     */
-    public static char[] getResourceContentsAsCharArray(final IFile file)
-            throws IOException, CoreException {
-        // Get encoding from file
-        Charset encoding = null;
-        try {
-            encoding = Charset.forName(file.getCharset());
-        } catch (final CoreException ce) {
-            // do not use any encoding
-        }
-        return getResourceContentsAsCharArray(file, encoding);
-    }
-
-    public static char[] getResourceContentsAsCharArray(final IFile file,
-            final Charset encoding) throws IOException, CoreException {
-        // Get resource contents
-        InputStream stream = null;
-        stream = new BufferedInputStream(file.getContents(true));
-        try {
-            return Util.getInputStreamAsCharArray(stream, -1, encoding);
-        } finally {
-            try {
-                stream.close();
-            } catch (final IOException e) {
-                // ignore
-            }
-        }
-    }
 
     /*
      * Returns the index of the most specific argument paths which is strictly
