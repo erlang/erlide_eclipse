d83c715e2b0ec2f8e0b39a50a2bc88a39d18fb2b
Vlad Dumitrescu <vladdu55@gmail.com>
2015-05-14 09:08:14 +0200

    check console issue

--- org.erlide.backend/src/org/erlide/backend/console/ConsoleEventHandler.java
+++ org.erlide.backend/src/org/erlide/backend/console/ConsoleEventHandler.java
@@ -1,18 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2009-2013 Vlad Dumitrescu and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available
- * at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009-2013 Vlad Dumitrescu and others. All rights reserved. This program
+ * and the accompanying materials are made available under the terms of the Eclipse Public
+ * License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Vlad Dumitrescu
+ * Contributors: Vlad Dumitrescu
  *******************************************************************************/
 package org.erlide.backend.console;
 
 import org.erlide.runtime.events.ErlEvent;
 import org.erlide.runtime.events.ErlangEventHandler;
 import org.erlide.runtime.shell.IBackendShell;
+import org.erlide.util.ErlLogger;
 
 import com.google.common.eventbus.Subscribe;
 
@@ -31,6 +30,10 @@ public class ConsoleEventHandler extends ErlangEventHandler {
             return;
         }
 
-        shell.add(event.getEvent());
+        try {
+            shell.add(event.getEvent());
+        } catch (Exception e) {
+            ErlLogger.error(e);
+        }
     }
 }
