ae553c1da8059b5ac33af987b57c299d74794ff7
Vlad Dumitrescu <vladdu55@gmail.com>
2016-05-09 18:28:21 +0200

    return the right version for a project's OTP library

--- org.erlide.runtime/xtend-gen/org/erlide/runtime/runtimeinfo/VersionLocator.java
+++ org.erlide.runtime/xtend-gen/org/erlide/runtime/runtimeinfo/VersionLocator.java
@@ -1,7 +1,7 @@
 package org.erlide.runtime.runtimeinfo;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
@@ -21,7 +21,16 @@ public class VersionLocator {
     Set<RuntimeInfo> _xblockexpression = null;
     {
       final List<RuntimeInfo> result = CollectionLiterals.<RuntimeInfo>newArrayList();
-      for (final RuntimeInfo info : runtimes) {
+      final Comparator<RuntimeInfo> _function = new Comparator<RuntimeInfo>() {
+        @Override
+        public int compare(final RuntimeInfo a, final RuntimeInfo b) {
+          RuntimeVersion _version = a.getVersion();
+          RuntimeVersion _version_1 = b.getVersion();
+          return _version.compareTo(_version_1);
+        }
+      };
+      List<RuntimeInfo> _sortWith = IterableExtensions.<RuntimeInfo>sortWith(runtimes, _function);
+      for (final RuntimeInfo info : _sortWith) {
         {
           final RuntimeVersion v = info.getVersion();
           boolean _isReleaseCompatible = v.isReleaseCompatible(vsn);
@@ -30,7 +39,6 @@ public class VersionLocator {
           }
         }
       }
-      Collections.reverse(result);
       for (final RuntimeInfo info_1 : runtimes) {
         {
           final RuntimeVersion v = info_1.getVersion();
@@ -46,13 +54,13 @@ public class VersionLocator {
           result.add(info_2);
         }
       }
-      final Function1<RuntimeInfo, Boolean> _function = new Function1<RuntimeInfo, Boolean>() {
+      final Function1<RuntimeInfo, Boolean> _function_1 = new Function1<RuntimeInfo, Boolean>() {
         @Override
         public Boolean apply(final RuntimeInfo it) {
           return Boolean.valueOf(it.isValid());
         }
       };
-      Iterable<RuntimeInfo> _filter = IterableExtensions.<RuntimeInfo>filter(result, _function);
+      Iterable<RuntimeInfo> _filter = IterableExtensions.<RuntimeInfo>filter(result, _function_1);
       _xblockexpression = IterableExtensions.<RuntimeInfo>toSet(_filter);
     }
     return _xblockexpression;
