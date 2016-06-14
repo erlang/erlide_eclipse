78c06a04b5ac2b2830f562f70448f5e9aa353ddd
Vlad Dumitrescu <vladdu55@gmail.com>
2016-06-14 16:52:32 +0200

    support css-configurable syntax highlighting (for dark theme)

--- org.erlide.ui/src/org/erlide/ui/prefs/TokenHighlight.java
+++ org.erlide.ui/src/org/erlide/ui/prefs/TokenHighlight.java
@@ -4,47 +4,39 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.StringConverter;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.RGB;
-import org.erlide.ui.prefs.PreferenceConstants.Color;
 import org.erlide.ui.prefs.plugin.ColoringPreferencePage;
 import org.osgi.service.prefs.Preferences;
 
 public enum TokenHighlight {
     //@formatter:off
-    DEFAULT(Color.BLACK.getColor(), SWT.NORMAL),
-    KEYWORD(new RGB(0xA0, 0x20, 0xF0), SWT.BOLD),
-    ATOM(Color.BLACK.getColor(), SWT.NORMAL),
-    MACRO(new RGB(0x5f, 0x9e, 0xA0), SWT.NORMAL),
-    ARROW(new RGB(0x00, 0x00, 0xff), SWT.NORMAL),
-    CHAR(new RGB(0xBc, 0x8f, 0x8F), SWT.NORMAL),
-    VARIABLE(new RGB(0xB8, 0x86, 0x0B), SWT.NORMAL),
-    INTEGER(new RGB(90, 90, 180), SWT.NORMAL),
-    FLOAT(Color.NAVY.getColor(), SWT.NORMAL),
+    DEFAULT,
+    KEYWORD,
+    ATOM,
+    MACRO,
+    ARROW,
+    CHAR,
+    VARIABLE,
+    INTEGER,
+    FLOAT,
 
-    COMMENT(new RGB(0xB2, 0x22, 0x22), SWT.NORMAL),
-    EDOC_TAG(new RGB(0x82, 0x22, 0x22), SWT.BOLD, "EDoc tag (in comments)"),
-    HTML_TAG(new RGB(0xB3, 0x6A, 0x6A), SWT.NORMAL, "HTML tag (in comments)"),
+    COMMENT,
+    EDOC_TAG("EDoc tag (in comments)"),
+    HTML_TAG("HTML tag (in comments)"),
 
-    STRING(new RGB(0xBC, 0x8f, 0x8F), SWT.NORMAL),
-    ESCAPE_TAG(new RGB(0xBC, 0x8f, 0x8F), SWT.BOLD, "Escaped chars (in strings)"),
-    TILDE_TAG(new RGB(0xBC, 0x8f, 0x8F), SWT.BOLD, "Format specifiers (in strings)");
+    STRING,
+    ESCAPE_TAG("Escaped chars (in strings)"),
+    TILDE_TAG("Format specifiers (in strings)");
     //@formatter:on
 
-    private final HighlightStyle defaultStyle;
     private final String displayName;
 
-    private TokenHighlight(final RGB color, final int styles) {
-        this(color, styles, null);
+    private TokenHighlight() {
+        this(null);
     }
 
-    private TokenHighlight(final RGB color, final int styles, final String displayName) {
-        defaultStyle = new HighlightStyle(color, styles);
+    private TokenHighlight(final String displayName) {
         this.displayName = displayName;
-    }
-
-    public HighlightStyle getDefaultStyle() {
-        return defaultStyle;
     }
 
     public String getName() {
@@ -120,4 +112,5 @@ public enum TokenHighlight {
         return key.substring(ColoringPreferencePage.COLORS_QUALIFIER.length(),
                 key.length() - ColoringPreferencePage.STYLE_KEY.length() - 1);
     }
+
 }
