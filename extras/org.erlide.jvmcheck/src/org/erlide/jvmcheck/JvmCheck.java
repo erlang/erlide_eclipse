7d874c765681ad0186843b14791f78ba1f0b4927
Vlad Dumitrescu <vladdu55@gmail.com>
2018-03-21 17:03:48 +0100

    fix java version detection

--- extras/org.erlide.jvmcheck/src/org/erlide/jvmcheck/JvmCheck.java
+++ extras/org.erlide.jvmcheck/src/org/erlide/jvmcheck/JvmCheck.java
@@ -18,74 +18,73 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 public class JvmCheck implements IStartup, JvmCheckConstants_Actual {
-	
+
 	public static String getJavaVersionProperty() {
 		return System.getProperty("java.version");
 	}
-	
-	public static int getJavaVersion() {
-	    String versionProperty = JvmCheck.getJavaVersionProperty();
-	    String[] versionSegments = versionProperty.split("\\.");
-	    
-	    if(versionSegments.length < 2) {
-	    	return -1;
-	    }
-        // v<9: 1.8...
-        // v>9: 9.x...
-        String javaVersionStr = versionSegments[0];
-        try {
-            int v = Integer.parseInt(javaVersionStr);
-            if (v == 1) {
-                javaVersionStr = versionSegments[1];
-                return Integer.parseInt(javaVersionStr);
-            }
-            return v;
-        } catch (NumberFormatException e) {
-            return -1;
-        }
-    }
 
-    @Override
+	public static int getJavaVersion() {
+		final String versionProperty = JvmCheck.getJavaVersionProperty();
+		final String[] versionSegments = versionProperty.split("\\.");
+
+		// v<9: 1.8...
+		// v>9: 9.x...
+		String javaVersionStr = versionSegments[0];
+		try {
+			final int v = Integer.parseInt(javaVersionStr);
+			if (v == 1) {
+				javaVersionStr = versionSegments[1];
+				return Integer.parseInt(javaVersionStr);
+			}
+			return v;
+		} catch (final NumberFormatException e) {
+			return -1;
+		}
+	}
+
+	@Override
 	public void earlyStartup() {
 		final int javaVersion = JvmCheck.getJavaVersion();
-		
-		if(javaVersion >= JvmCheckConstants_Actual.REQUIRED_JAVA_VERSION)
+
+		if (javaVersion >= JvmCheckConstants_Actual.REQUIRED_JAVA_VERSION) {
 			return;
-		
-		// Show error message to the user, because the platform just silently fails. 
+		}
+
+		// Show error message to the user, because the platform just silently fails.
 		// See: https://bugs.eclipse.org/bugs/show_bug.cgi?id=417336
-		
+
 		Display.getDefault().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				Shell activeShell = JvmCheck.getActiveWorkbenchShell();
-				
-				String message = "Could not start " + JvmCheckConstants_Actual.FEATURE_NAME + " because Java version is: " + javaVersion
-						+ "\nVersion " + JvmCheckConstants_Actual.REQUIRED_JAVA_VERSION + " is required";
-				
+				final Shell activeShell = JvmCheck.getActiveWorkbenchShell();
+
+				final String message = "Could not start " + JvmCheckConstants_Actual.FEATURE_NAME
+						+ " because Java version is: " + javaVersion + "\nVersion "
+						+ JvmCheckConstants_Actual.REQUIRED_JAVA_VERSION + " or later is required";
+
 				System.err.println(message);
-				
-				if(activeShell == null) {
+
+				if (activeShell == null) {
 					return;
 				}
 				MessageDialog.openError(activeShell, "Error", message);
 			}
 		});
-	
+
 	}
-	
+
 	/** Gets the active workbench window. */
 	public static IWorkbenchWindow getActiveWorkbenchWindow() {
 		return PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 	}
-	
+
 	/** Gets the active workbench shell. */
 	public static Shell getActiveWorkbenchShell() {
-		IWorkbenchWindow window = JvmCheck.getActiveWorkbenchWindow();
-		if(window != null) {
+		final IWorkbenchWindow window = JvmCheck.getActiveWorkbenchWindow();
+		if (window != null) {
 			return window.getShell();
 		}
 		return null;
 	}
-	
+
 }
