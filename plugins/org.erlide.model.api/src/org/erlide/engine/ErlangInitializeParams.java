47cc38fa13abddbbd8930fb628f89842c96bb842
Vlad Dumitrescu <vladdu55@gmail.com>
2019-06-21 22:07:16 +0200

    Remove lsp4j references
    We will add them when needed, keep it clean for now

--- plugins/org.erlide.model.api/src/org/erlide/engine/ErlangInitializeParams.java
+++ plugins/org.erlide.model.api/src/org/erlide/engine/ErlangInitializeParams.java
@@ -1,8 +1,6 @@
 package org.erlide.engine;
 
-import org.eclipse.lsp4j.InitializeParams;
-
-public abstract class ErlangInitializeParams extends InitializeParams {
+public abstract class ErlangInitializeParams {
 
     public abstract String getStateDir();
 
